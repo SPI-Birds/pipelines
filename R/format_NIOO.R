b6eafe582a6b466c7486a4e5c060b1ead067bb7f
Unknown <liam.bailey@liamdbailey.com>
2018-11-02 11:59:37 +0100

    Slight changes to incorporate Stefan comments
    - Ensure all columns are camel case
    - Save files now specify that they are NIOO data

--- R/format_NIOO.R
+++ R/format_NIOO.R
@@ -1,14 +1,14 @@
-#' Construct standard summary for Hoge Veluwe population
+#' Construct standard summary for main NIOO populations
 #'
 #' @param db Location of database
+#' @param Species Which species should be included? (default to great and blue tit)
+#' @param path Location where output files will be saved.
 #'
-#' @return
+#' @return Generates 5 .csv files with data in a standard format.
 #' @export
-#' @import dbplyr
+#' @import dplyr
 #' @import DBI
-#' @import magrittr
-#'
-#' @examples
+#' @import purrr
 
 format_NIOO <- function(db = NULL, Species = c(14620, 14640), path = "."){
 
@@ -45,22 +45,13 @@ format_NIOO <- function(db = NULL, Species = c(14620, 14640), path = "."){
   main_sites <- c("Buunderkamp", "Lichtenbeek", "Westerheide", "Hoge Veluwe", "Warnsborn", "Vlieland", "Oosterhout", "Liesbosch")
 
   #Extract the area table
+  #Areas are inside populations (e.g. Hoge Veluwe has multiple area codes)
   Area_data     <- tbl(connection, "dbo_tbl_Area") %>%
     collect() %>%
     #Only include areas that correspond to the main sites specified above
-    filter(grepl(paste(main_sites, collapse = "|"), Name))
-
-  #Extract the location table
-  Location_data <- tbl(connection, "dbo_tbl_Location") %>%
-    #Filter out only those locations that are within the main areas
-    filter(AreaID %in% Area_data$ID) %>%
-    collect() %>%
-    #For now, we use the full UserPlaceName e.g. HV.0506.0 instead of splitting it up.
-    #This is probably easier because it will make sure that every nestbox ID is unique.
-    #Join in the names from the area table
-    left_join(dplyr::select(Area_data, AreaID = ID, Name), by = "AreaID") %>%
+    filter(grepl(paste(main_sites, collapse = "|"), Name)) %>%
     #Change names to just use the three letter codes of the sites
-    mutate(Name = purrr::map_chr(.x = Name, .f = function(x, main_sites){
+    mutate(PopID = purrr::map_chr(.x = Name, .f = function(x, main_sites){
 
       site_name <- main_sites[unlist(lapply(main_sites, function(y){
 
@@ -72,6 +63,15 @@ format_NIOO <- function(db = NULL, Species = c(14620, 14640), path = "."){
 
     }, main_sites))
 
+  #Extract the nestbox locations.
+  #Nestbox locations are WITHIN areas, which are within populations.
+  Locations <- tbl(connection, "dbo_tbl_Location") %>%
+    collect() %>%
+    #For now, we use the full UserPlaceName e.g. HV.0506.0 instead of splitting it up.
+    #This is probably easier because it will make sure that every nestbox ID is unique.
+    #Join in the names from the area table
+    left_join(dplyr::select(Area_data, AreaID = ID, PopID), by = "AreaID")
+
   ################
   # CAPTURE DATA #
   ################
@@ -82,15 +82,24 @@ format_NIOO <- function(db = NULL, Species = c(14620, 14640), path = "."){
 
   print("Compiling capture information...")
 
+  #Capture data includes all times an individual was captured (with measurements like mass, tarsus etc.).
+  #This will include first capture as nestling (for residents)
+  #This means there will be multiple records for a single individual.
   Capture_data <- tbl(connection, "dbo_tbl_Capture") %>%
-    #Join in CaptureData table with weight, tarsus and wing_length
     select(CaptureID = ID, IndvID = Individual, CaptureLocation, ReleaseLocation, CaptureType) %>%
+    #Join in weight, tarsus and wing_length from secondary capture data table.
     left_join(tbl(connection, "dbo_vw_MI_CaptureCaptureData") %>%
                 select(CaptureID, CaptureDate, SpeciesID, Weight, Tarsus, Wing_Length), by = "CaptureID") %>%
     #Filter just GT and BT
     filter(SpeciesID %in% Species) %>%
     #Create a new column that has the 2 letter ID for species
-    mutate(Species = ifelse(SpeciesID == 14620, "BT", ifelse(SpeciesID == 14640, "GT", NA))) %>%
+    #Include information about the tarsus length measurement method
+    mutate(Species = ifelse(SpeciesID == 14620, "BT", ifelse(SpeciesID == 14640, "GT", NA)),
+           TarsusMethod = NA) %>%
+    #Join in info on the capture type. This includes:
+    #-Measurements of eggs
+    #-Measurements of nestlings/juveniles (i.e. with down)
+    #-Measurements of any bird that is already ringed
     left_join(tbl(connection, "dbo_tl_CaptureType") %>%
                 select(CaptureType = ID, Name), by = "CaptureType") %>%
     #Remove only the basic info we need
@@ -103,15 +112,15 @@ format_NIOO <- function(db = NULL, Species = c(14620, 14640), path = "."){
     # -Wing_Length
     # -Age
     select(CaptureDate, Species, Type = Name, IndvID, CaptureLocation,
-           ReleaseLocation, Weight, Tarsus, Wing_Length) %>%
+           ReleaseLocation, Weight, Tarsus, TarsusMethod, WingLength = Wing_Length) %>%
     #Arrange by species, indv and date
     arrange(Species, IndvID, CaptureDate) %>%
     collect() %>%
-    #Include three letter population codes for capture and release location
-    left_join(dplyr::select(Location_data, CaptureLocation = ID, CaptureName = Name), by = "CaptureLocation") %>%
-    left_join(dplyr::select(Location_data, ReleaseLocation = ID, ReleaseName = Name), by = "ReleaseLocation") %>%
+    #Include three letter population codes for both the capture and release location (some individuals may have been translocated e.g. cross-fostering)
+    left_join(dplyr::select(Locations, CaptureLocation = ID, CapturePop = PopID), by = "CaptureLocation") %>%
+    left_join(dplyr::select(Locations, ReleaseLocation = ID, ReleasePop = PopID), by = "ReleaseLocation") %>%
     #Arrange columns
-    select(CaptureDate, Species, Type, IndvID, CaptureLocation = CaptureName, ReleaseLocation = ReleaseName, Weight, Tarsus, WingLength = Wing_Length)
+    select(CaptureDate, Species, Type, IndvID, CapturePop, ReleasePop, Mass = Weight, Tarsus, TarsusMethod, WingLength)
 
 
   ###################
@@ -120,17 +129,20 @@ format_NIOO <- function(db = NULL, Species = c(14620, 14640), path = "."){
 
   print("Compiling individual information...")
 
+  #This is a summary of each individual and general lifetime information (e.g. sex, resident/immigrant)
+
+  #Create table with description of sex codes
   Sex_data <- tbl(connection, "dbo_tl_Sexe") %>%
-    #Just select the Sex ID and its description
+    #Keep just the Sex ID and its description
     select(Sexe = ID, Sex = Description)
 
-  #Determine the first capture type of each individual (i.e. nestling or breeding bird)
+  #Determine the first capture type of each individual (i.e. nestling/egg or breeding bird)
   First_capture <- Capture_data %>%
     group_by(IndvID) %>%
     summarise(Status = ifelse(any(Type != "Ringed"), "Resident", "Immigrant"))
 
-  #Subset only broods of designated species
   Indv_data   <- tbl(connection, "dbo_tbl_Individual") %>%
+    #Subset only chosen species
     filter(SpeciesID %in% Species) %>%
     #Create a new column that has the 2 letter ID for species
     mutate(Species = ifelse(SpeciesID == 14620, "BT", "GT")) %>%
@@ -149,14 +161,15 @@ format_NIOO <- function(db = NULL, Species = c(14620, 14640), path = "."){
     #Remove old sex info
     select(-Sexe) %>%
     collect() %>%
+    #Join in info on when the individual was first captured (i.e. do we know if it was born in the population or not?)
     left_join(First_capture, by = "IndvID") %>%
-    #Use map to sort our brood laid and brood fledged
+    #Use map to sort out brood laid and brood fledged
     mutate(BroodIDLaid = purrr::map2_dbl(.x = BroodID, .y = GeneticBroodID,
                                           #If there is no genetic brood listed but there is a regular broodID, assume these are the same
                                           .f = ~ifelse(is.na(.y) & !is.na(.x), .x, .y)),
 
            BroodIDFledged = purrr::map2_dbl(.x = BroodID, .y = GeneticBroodID,
-                                             #If there is a genetic broodID listed by no regular brood ID make them the same.
+                                          #If there is a genetic broodID listed by no regular brood ID assume these are the same.
                                           .f = ~ifelse(!is.na(.y) & is.na(.x), .y, .x))) %>%
     select(BroodIDLaid, BroodIDFledged, Species, IndvID, RingNumber, RingYear, RingAge, Status, Sex)
 
@@ -168,8 +181,8 @@ format_NIOO <- function(db = NULL, Species = c(14620, 14640), path = "."){
 
   print("Compiling brood information...")
 
-  #Subset only broods of designated species
   Brood_data  <- tbl(connection, "dbo_tbl_Brood") %>%
+    #Subset only broods of designated species
     filter(BroodSpecies %in% Species & BroodLocationID %in% Location_data$ID) %>%
     #Create a new column that has the 2 letter ID for species
     mutate(Species = ifelse(BroodSpecies == 14620, "BT", "GT")) %>%
@@ -190,12 +203,12 @@ format_NIOO <- function(db = NULL, Species = c(14620, 14640), path = "."){
     #Collect data so we can use map functions
     #This needs to be done because we are applying our function rowwise.
     collect() %>%
-    #Join in ID numbers from the individual table above
+    #Join in ID numbers for the parents of the brood from the individual table above
     left_join(select(Indv_data, Female_ring = RingNumber, FemaleID = IndvID), by = "Female_ring") %>%
     left_join(select(Indv_data, Male_ring = RingNumber, MaleID = IndvID), by = "Male_ring") %>%
-    #Join location info (inlcuding site ID and nestbox ID)
-    left_join(dplyr::select(Location_data, BroodLocation  = ID, NestboxName = UserPlaceName, Name), by = "BroodLocation") %>%
-    select(BroodYear, Species, PopID = Name, BroodID, NestboxName, FemaleID, MaleID, ClutchType, LayingDate, ClutchSize, HatchDate, NumberHatched, FledgeDate, NumberFledged) %>%
+    #Join location info (including site ID and nestbox ID)
+    left_join(dplyr::select(Locations, BroodLocation  = ID, NestboxName = UserPlaceName, PopID), by = "BroodLocation") %>%
+    select(BroodYear, Species, PopID, BroodID, NestboxName, FemaleID, MaleID, ClutchType, LayingDate, ClutchSize, HatchDate, NumberHatched, FledgeDate, NumberFledged) %>%
     arrange(PopID, BroodYear, Species)
 
   ################
@@ -204,17 +217,20 @@ format_NIOO <- function(db = NULL, Species = c(14620, 14640), path = "."){
 
   print("Compiling nestbox information...")
 
+  #Extract information on nestbox locations
   Nestbox_data <- tbl(connection, "dbo_tbl_NestboxAppearance") %>%
     collect() %>%
-    left_join(select(Location_data, Location = ID, NestboxName = UserPlaceName, Latitude, Longitude, Name),
+    #Join together information on the nestbox locations (e.g. latitude, longitude, nestbox name) and information on each nestbox that was there (e.g. how long before it was replaced).
+    #This is necessary because one nestbox location could have multiple nestboxes erected at it over the study period.
+    left_join(select(Locations, Location = ID, NestboxName = UserPlaceName, Latitude, Longitude, PopID),
               by = "Location") %>%
-    select(NestboxName, NestboxID = ID, PopID = Name, Latitude, Longitude, StartYear, EndYear)
+    select(NestboxName, NestboxID = ID, PopID, Latitude, Longitude, StartYear, EndYear)
 
   ###################
   # POPULATION DATA #
   ###################
 
-  #Create a final data frame that has summary statistics of the population.
+  #Create a final data frame that has summary information about each population.
 
   print("Compiling population summary information...")
 
@@ -235,15 +251,15 @@ format_NIOO <- function(db = NULL, Species = c(14620, 14640), path = "."){
 
   print("Saving .csv files...")
 
-  write.csv(x = Brood_data, file = paste0(path, "\\Brood_data.csv"), row.names = F)
+  write.csv(x = Brood_data, file = paste0(path, "\\Brood_data_NIOO.csv"), row.names = F)
 
-  write.csv(x = Indv_data %>% select(-RingNumber), file = paste0(path, "\\Indv_data.csv"), row.names = F)
+  write.csv(x = Indv_data %>% select(-RingNumber), file = paste0(path, "\\Indv_data_NIOO.csv"), row.names = F)
 
-  write.csv(x = Capture_data, file = paste0(path, "\\Capture_data.csv"), row.names = F)
+  write.csv(x = Capture_data, file = paste0(path, "\\Capture_data_NIOO.csv"), row.names = F)
 
-  write.csv(x = Nestbox_data, file = paste0(path, "\\Nestbox_data.csv"), row.names = F)
+  write.csv(x = Nestbox_data, file = paste0(path, "\\Nestbox_data_NIOO.csv"), row.names = F)
 
-  write.csv(x = Pop_data, file = paste0(path, "\\Summary_data.csv"), row.names = F)
+  write.csv(x = Pop_data, file = paste0(path, "\\Summary_data_NIOO.csv"), row.names = F)
 
   time <- difftime(Sys.time(), start_time, units = "sec")
 
