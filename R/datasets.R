c233cbf99d836ed143184a2e35fbcb284c39ffd4
Liam D. Bailey <liam.bailey@liamdbailey.com>
2019-05-20 17:39:18 +0200

    Add dataset to convert EUring species codes to letter codes

---
+++ R/datasets.R
@@ -0,0 +1,10 @@
+#' Species EUring codes and letter codes
+#'
+#' Letter codes to easily identify species (English names)
+#' @format A data frame with 6 rows and 2 variables
+#' \describe{
+#'   \item{SpeciesID}{Species EUring code.}
+#'   \item{Code}{Species letter code.}
+#'   }
+#'@name Species_codes
+NULL
