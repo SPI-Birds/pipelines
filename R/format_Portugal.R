e833e478e4828cdb649e67d74b041b2aed8dac51
Liam D. Bailey <liam.bailey@liamdbailey.com>
2019-05-29 11:16:59 +0200

    Add comments to Portugal pipeline
    Include more detailed comments for greater readability of Portugal pipeline

--- R/format_Portugal.R
+++ R/format_Portugal.R
@@ -9,6 +9,15 @@
 #'
 #' \strong{NumberFledged}: This population has no estimation of actual fledgling numbers.
 #' The last time nests are counted is 14 days post hatching. We use this as an estimation of fledling numbers.
+#'
+#' \strong{MinAge, RingAge}: Any individual caught as a chick was assumed to
+#' have a EUring code of 1: 'Pullus: nestling or chick, unable to fly freely, still able to be caught by hand.'
+#' Any individual not caught as a chick was assumed to have a EUring code of 4:
+#' 'After first full year: year of hatching unknown'.
+#'
+#' \strong{ClutchType_observed}: In the raw data, there is no distinction between 'second' and 'replacement' clutches.
+#' Any clutch recorded as '2nd' is assumed to be a 'second' clutch under our definitions.
+#' 'ClutchType_calc' may give a more appropriate estimate of clutch type for this data.
 #' @param db Location of database file.
 #' @param Species A numeric vector. Which species should be included (EUring codes)? If blank will return all major species (see details below).
 #' @param path Location where output csv files will be saved.
@@ -24,6 +33,7 @@ format_Portugal <- function(db = NULL,
                             Species = NULL,
                             path = "."){
 
+  #Find database path
   if(is.null(db)){
 
     message("Please select a database location...")
@@ -32,17 +42,19 @@ format_Portugal <- function(db = NULL,
 
   }
 
-  #Record start time to estimate processing time.
+  #Record start time to provide processing time to the user.
   start_time <- Sys.time()
 
   #Read in data with readxl
   all_data <- read_excel(db) %>%
     #Clean all names with janitor
     janitor::clean_names(case = "upper_camel") %>%
-    #Change species to "GT" because it's only GT
+    #Change species to "GT" because it's only GT in Choupal
     mutate(Species = "GT") %>%
     #BroodIDs are not unique (they are repeated each year)
     #We need to create unique IDs for each year
+    #Convert capture date into a date object (not Julian days)
+    #Need this to order captures chronologically
     mutate(BroodID = paste(Year, BroodId, sep = "_"),
            CaptureDate = lubridate::ymd(paste0(Year, "-01-01")) + JulianDate)
 
@@ -52,42 +64,56 @@ format_Portugal <- function(db = NULL,
 
   message("Compiling capture information...")
 
+  #Take all data and add population/plot info
+  #There is only one population/plot
   Capture_data <- all_data %>%
     mutate(CapturePopID = "CHO", ReleasePopID = "CHO",
            CapturePlot = NA, ReleasePlot = NA) %>%
     #Determine age at first capture for every individual
+    #First arrange the data chronologically within each individual
     arrange(Ring, CaptureDate) %>%
+    #Then, for each individual, determine the first age and year of capture
     group_by(Ring) %>%
     mutate(FirstAge = first(Age),
            FirstYear = first(Year)) %>%
     ungroup() %>%
-    #Determine age at capture using EUring
+    #Calculate age at each capture using EUring codes
     mutate(MinAge = purrr::pmap_dbl(.l = list(Age = .$FirstAge,
                                               Year1 = .$FirstYear,
                                               YearN = .$Year),
                                     .f = function(Age, Year1, YearN){
 
+                                      # If age at first capture is unknown
+                                      # we cannot determine age at later captures
                                       if(is.na(Age)){
 
                                         return(NA)
 
                                       }
 
+                                      #Determine number of years since first capture...
                                       diff_yr <- (Year1 - YearN)
 
-                                      #If it was not caught as a chick
+                                      #If it was not caught as a chick...
                                       if(Age != "C"){
 
+                                        #Use categories where age is uncertain
+                                        #(6, 8)
                                         return(4 + 2*diff_yr)
 
                                       } else {
 
+                                        #If it was caught as a chick
                                         if(diff_yr == 0){
 
+                                          #Make the age at first capture 1 (nestling/unable to fly)
+                                          #N.B. There is no distinction between chick and fledgling in the data
                                           return(1)
 
                                         } else {
 
+
+                                          #Otherwise, use categories where age is certain (5, 7, etc.)
                                           return(3 + 2*diff_yr)
 
                                         }
@@ -95,6 +121,7 @@ format_Portugal <- function(db = NULL,
                                       }
 
                                     })) %>%
+    #Select out only those columns we need.
     select(CaptureDate, CaptureTime = Time,
            IndvID = Ring, Species,
            CapturePopID, CapturePlot,
@@ -114,56 +141,70 @@ format_Portugal <- function(db = NULL,
   #Subset only those records with a brood ID.
   ##THERE IS ONE RECORD THAT WAS MEANT TO BE CAUGHT AT A BOX BUT NO ID RECORDED
   ##NEED TO CHECK THIS!!
-  #We're not interested in chick captures
-  #These contain no brood data
   Brood_data <- all_data %>%
     filter(!is.na(BroodId))
 
-  #Determine adults caught on the brood
-  #Assume these are the parents
-  #Reshape data so that sex is a column not a row
+  #Identify any adults caught on the brood
+  #Reshape data so that sex is a column not a row (i.e. MaleID, FemaleID)
   Parents <- Brood_data %>%
+    #Remove all records with chicks
     filter(Age != "C") %>%
+    #Select only the Brood, Individual Id and their sex
     select(BroodID, Ring, Sex) %>%
     #'No ringed/no ring' becomes NA
     mutate(Ring = map_chr(.x = Ring, .f = ~ifelse(grepl(pattern = "ring", .x), NA, .x))) %>%
+    #Reshape data so that we have a MaleID and FemaleID column
+    #Rather than an individual row for each parent
     reshape2::melt(id = c("BroodID", "Sex")) %>%
     reshape2::dcast(BroodID ~ Sex) %>%
     rename(FemaleID = `F`, MaleID = `M`)
 
   #Determine whether clutches are 2nd clutch
   ClutchType_obsv <- Brood_data %>%
+    #For each brood, if a clutch is listed as '2nd' make it 'second' otherwise 'first'.
+    #For ClutchType_observed we are excluding 'replacement' as we don't have enough info.
     group_by(BroodID) %>%
     summarise(ClutchType_observed = ifelse("2nd" %in% SecondClutch, "second", "first"))
 
   Brood_data <- Brood_data %>%
+    #Join in information on parents and clutch type
     left_join(Parents, by = "BroodID") %>%
     left_join(ClutchType_obsv, by = "BroodID") %>%
     #Now we can melt and reshape our data
     #Remove columns that do not contain relevant brood info
     select(-CodeLine:-Ring, -JulianDate:-StanderdisedTime, -TrapingMethod,
            -BroodId:-Smear, -MeanEggWeight:-NºEggsWeighted) %>%
+    #Turn all remaining columns to characters
+    #melt/cast requires all values to be of the same type
     mutate_all(as.character) %>%
+    #Melt and cast data so that we return the first value of relevant brood data
+    #e.g. laying date, clutch size etc.
+    ##N.B. CHECK THIS TO MAKE SURE THAT THE FIRST RECORD IS ALWAYS CORRECT/REPRESENTATIVE
     melt(id = c("BroodID", "Species", "Year", "Site", "Box", "FemaleID", "MaleID")) %>%
     dcast(BroodID + Species + Year + Site + Box + FemaleID + MaleID ~ ..., fun.aggregate = first) %>%
+    #Any case where chicks are not counted at 14d make as 0
     rowwise() %>%
     mutate(NoChicksOlder14D = ifelse(is.na(NoChicksOlder14D), 0, as.numeric(NoChicksOlder14D))) %>%
     ungroup() %>%
-    #Determine the 30 day cut-off for all species
-    ### NEED TO GO THROUGH AND DETERMINE WHETHER THE 30 DAY CUT OFF IS REASONABLE FOR ALL SPECIES
-    ### LOOK AT THIS LATER
-    group_by(Year, Species) %>%
+    #Determine the 30 day cut-off for each year
+    group_by(Year) %>%
     mutate(cutoff = tryCatch(expr = min(as.numeric(LayingDateJulian), na.rm = T) + 30,
                              warning = function(...) return(NA))) %>%
-    # Determine brood type for each nest based on female ID
-    arrange(Year, Species, FemaleID, as.numeric(LayingDateJulian)) %>%
-    group_by(Year, Species, FemaleID) %>%
+    # Determine cumulative fledgling information for each clutch
+    # Arrange data chronologically for each female in each year
+    arrange(Year, FemaleID, as.numeric(LayingDateJulian)) %>%
+    group_by(Year, FemaleID) %>%
+    #Determine the cumulative number of fledglings produced
+    #up until the current clutch
+    #Use this to determine if a clutch is second/replacement
     mutate(total_fledge = cumsum(NoChicksOlder14D), row = 1:n()) %>%
     ungroup()
 
+  #Create a progress bar
   clutchtype <- dplyr::progress_estimated(n = nrow(Brood_data))
 
   Brood_data <- Brood_data %>%
+    #For every clutch, go through and calculate the clutch type.
     mutate(ClutchType_calc = purrr::pmap_chr(.l = list(rows = .$row,
                                                        femID = .$FemaleID,
                                                        cutoff_date = .$cutoff,
@@ -174,9 +215,8 @@ format_Portugal <- function(db = NULL,
 
                                                clutchtype$tick()$print()
 
-                                               #Firstly, check if the nest has a LD
-                                               #If not, we cannot calculate BroodType
-
+                                               #Firstly, check if the nest has laying date infor.
+                                               #If not, we cannot calculate ClutchType, return NA
                                                if(is.na(LD)){
 
                                                  return(NA)
@@ -189,23 +229,25 @@ format_Portugal <- function(db = NULL,
                                                #Therefore it can only be first or replacement (based on 30d rule)
                                                if(is.na(femID)){
 
+                                                 #If the clutch was laid after the cut-off it's a replacement
                                                  if(LD > cutoff_date){
 
                                                    return("replacement")
 
                                                  } else {
 
+                                                   #Otherwise, it's a first clutch
                                                    return("first")
 
                                                  }
 
                                                }
 
-                                               #If she is banded, then we need to apply all rules
+                                               #If the female is banded, then we need to apply all rules
                                                #If it's the first nest recorded for this female in this year...
                                                if(rows == 1){
 
-                                                 #If it doesn't meet the 30 day rule, then name it as replacement
+                                                 #...and it doesn't meet the 30 day rule, then it is a replacement
                                                  if(LD > cutoff_date){
 
                                                    return("replacement")
@@ -217,7 +259,7 @@ format_Portugal <- function(db = NULL,
 
                                                  }
 
-                                                 #If it's NOT the first nest of the season for this female
+                                               #If it's NOT the first nest of the season for this female
                                                } else {
 
                                                  #If there have been no fledglings before this point..
@@ -236,27 +278,33 @@ format_Portugal <- function(db = NULL,
                                                }
 
                                              })) %>%
+    #Add in population/plot info
+    #Convert LayingDate and HatchDate to date objects
     mutate(PopID = "CHO", Plot = NA,
-           FledgeDate = NA) %>%
+           FledgeDate = NA,
+           HatchDate = lubridate::ymd(paste0(Year, "-01-01")) + as.numeric(HatchingDateJulian),
+           LayingDate = lubridate::ymd(paste0(Year, "-01-01")) + as.numeric(LayingDateJulian)) %>%
     #Select relevant columns and rename
     select(SampleYear = Year, Species, PopID, Plot,
            LocationID = Box, BroodID, FemaleID, MaleID,
            ClutchType_observed,
-           ClutchType_calc, LayingDate = LayingDateJulian,
+           ClutchType_calc, LayingDate,
            ClutchSize = FinalClutchSize,
-           HatchDate = HatchingDateJulian, BroodSize = NoChicksHatched,
+           HatchDate, BroodSize = NoChicksHatched,
            FledgeDate,
            NumberFledged = NoChicksOlder14D)
 
-  #For Capture_data, subset only those chicks that were 14 - 16 days when captured.
+  #Finally, we add in average mass and tarsus measured for all chicks at 14d
+  #From Capture_data, subset only those chicks that were 14 - 16 days when captured.
   avg_measure <- all_data %>%
     filter(!is.na(ChickAge) & ChickAge != "na") %>%
     mutate(ChickAge = as.numeric(ChickAge)) %>%
     filter(between(ChickAge, 14, 16)) %>%
+    #For every brood, determine the average mass and tarsus length
     group_by(BroodID) %>%
     summarise(AvgMass = mean(Weight, na.rm = T), AvgTarsus = mean(Tarsus, na.rm = T))
 
-  #Add this data to the Brood_data
+  #Add AvgMass and AvgTarsus to the Brood_data tibble
   Brood_data <- left_join(Brood_data, avg_measure, by = "BroodID")
 
   ###################
@@ -267,16 +315,24 @@ format_Portugal <- function(db = NULL,
 
   #Determine first age, brood, and ring year of each individual
   Indv_data <- all_data %>%
+    #Arrange data for each individual chronologically
     arrange(Ring, CaptureDate) %>%
+    #Replace 'na' with NA in Sex
     mutate(Sex = na_if(x = Sex, y = "na")) %>%
+    #For every individual
     group_by(Ring) %>%
+    #Determine the first recorded broodID, year and age.
+    #Determine if there were any records where sex was identified.
     summarise(FirstBrood = first(BroodID),
               FirstYr = first(Year),
               FirstAge = first(Age),
               Sex = ifelse(all(is.na(Sex)), "U",
                            ifelse(any(Sex %in% "M"), "M",
                                   ifelse(any(Sex %in% "F"), "F", NA)))) %>%
-    mutate(Species = "GT", PopID = "CHO",
+    #Add population ID
+    mutate(PopID = "CHO",
+           #Only assign a brood ID if they were first caught as a chick
+           #Otherwise, the broodID will be their first clutch as a parent
            BroodIDLaid = purrr::pmap_chr(.l = list(FirstBrood = .$FirstBrood,
                                                                    FirstAge = .$FirstAge),
                                                          .f = function(FirstBrood, FirstAge){
@@ -292,7 +348,11 @@ format_Portugal <- function(db = NULL,
                                                            }
 
                                                          }),
+           #We have no information on cross-fostering, so we assume the brood laid and ringed are the same
            BroodIDRinged = BroodIDLaid,
+           #Determine age at ringing with EUring code.
+           #If caught as a chick assign 1: Pallus.
+           #If caught as adult assign 4: >1yo, exact age unknown.
            RingAge = purrr::map_dbl(.x = .$FirstAge,
                                     .f = function(.x){
 
@@ -315,13 +375,13 @@ format_Portugal <- function(db = NULL,
 
   message("Compiling nestbox information...")
 
-  Nestbox_data <- all_data %>%
-    mutate(LocationID = Box,
-           NestboxID = Box,
+  #There are no coordinates or box type information
+  #Nestbox data is therefore just
+  Nestbox_data <- tibble(LocationID = unique(all_data$Box),
+           NestboxID = unique(all_data$Box),
            NestBoxType = NA, PopID = "CHO",
            Latitude = NA, Longitude = NA,
-           StartYear = NA, EndYear = NA) %>%
-    select(LocationID:EndYear)
+           StartYear = NA, EndYear = NA)
 
   message("Saving .csv files...")
 
