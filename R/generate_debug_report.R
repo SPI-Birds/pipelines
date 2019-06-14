1c695fde2b90d0559c7e963f8f8427e3098d58e7
Unknown <liam.bailey@liamdbailey.com>
2019-06-14 15:03:32 +0200

    Create debug report functions
    These functions generate plots and .csv files to show summaries of the generated data. This can be used to quickly check for any issues that may arise from building the pipelines.

---
+++ R/generate_debug_report.R
@@ -0,0 +1,95 @@
+#' Generate a debug report for a pipeline
+#'
+#' Generates a debug report which include histograms of all continuous variables
+#' and unique values for (some) categorical variable.
+#' @param path File path where debug report will be created.
+#' @param Pop Name of the population being debugged.
+#' @param Brood_data A table containing the brood data.
+#' @param Capture_data A table containing the capture data.
+#' @param Indv_data A table containing the individual data.
+#'
+#' @return Creates a directory 'debug_report' which contains .csv and .pdf files
+#'   with debug information.
+#' @export
+
+generate_debug_report <- function(path, Pop, Brood_data, Capture_data, Indv_data){
+
+  #Generate the debug folder
+  if (!dir.exists(paste0(path, "\\debug_report"))) {
+    dir.create(paste0(path, "\\debug_report"), recursive = TRUE)
+  }
+
+  ### DEBUG BROOD DATA ###
+
+  Brood_data_summary <- Brood_data %>%
+    select(Species, ClutchType_observed, ClutchType_calc) %>%
+    summarise_all(~paste(unique(.x), collapse = "/"))
+
+  #Turn all dates into April days
+  Brood_data <- Brood_data %>%
+    mutate_if(.predicate = ~class(.x) == "Date", .funs = function(.x){
+
+      as.numeric(.x - lubridate::ymd(paste(lubridate::year(.x), "01", "04")))
+
+    })
+
+  #Create a list of all histograms for continuous variables
+  Brood_plots <- purrr::map(.x = c("LayingDate", "ClutchSize", "HatchDate", "BroodSize", "FledgeDate", "NumberFledged", "AvgMass", "AvgTarsus"),
+                             .f = ~plot_debug_hist(table = Brood_data, variable = .x))
+
+  write.csv(Brood_data_summary, file = paste0(path, "\\debug_report\\Brood_summary", Pop, ".csv"))
+
+  pdf(file = paste0(path, "\\debug_report\\Brood_summary.pdf"), width = 7, height = 6)
+  purrr::map(.x = Brood_plots, .f = print)
+  dev.off()
+
+  ### DEBUG CAPTURE DATA ###
+
+  Capture_data_summary <- Capture_data %>%
+    select(Species, CaptureTime) %>%
+    summarise_all(~paste(unique(.x), collapse = "/"))
+
+  #Turn all dates into April days
+  Capture_data <- Capture_data %>%
+    mutate_if(.predicate = ~class(.x) == "Date", .funs = function(.x){
+
+      as.numeric(.x - lubridate::ymd(paste(lubridate::year(.x), "01", "04")))
+
+    })
+
+  #Create a list of all histograms for continuous variables
+  Capture_plots <- purrr::map(.x = c("Mass", "Tarsus", "WingLength", "Age_obsv", "Age_calc", "ChickAge"),
+                            .f = ~plot_debug_hist(table = Capture_data, variable = .x))
+
+  write.csv(Capture_data_summary, file = paste0(path, "\\debug_report\\Capture_summary", Pop, ".csv"))
+
+  pdf(file = paste0(path, "\\debug_report\\Capture_summary.pdf"), width = 7, height = 6)
+  purrr::map(.x = Capture_plots, .f = print)
+  dev.off()
+
+  ### DEBUG INDIVIDUAL DATA ###
+
+  Indv_data_summary <- Indv_data %>%
+    select(Species, Sex) %>%
+    summarise_all(~paste(unique(.x), collapse = "/"))
+
+  #Turn all dates into April days
+  Indv_data <- Indv_data %>%
+    ungroup() %>%
+    mutate_if(.predicate = ~class(.x) == "Date", .funs = function(.x){
+
+      as.numeric(.x - lubridate::ymd(paste(lubridate::year(.x), "01", "04")))
+
+    })
+
+  #Create a list of all histograms for continuous variables
+  Indv_plots <- purrr::map(.x = c("RingYear", "RingAge"),
+                            .f = ~plot_debug_hist(table = Indv_data, variable = .x))
+
+  write.csv(Indv_data_summary, file = paste0(path, "\\debug_report\\Indv_summary", Pop, ".csv"))
+
+  pdf(file = paste0(path, "\\debug_report\\Indv_summary.pdf"), width = 7, height = 6)
+  purrr::map(.x = Indv_plots, .f = print)
+  dev.off()
+
+}
---
+++ R/plot_debug_hist.R
@@ -0,0 +1,47 @@
+#' Generate histograms of continuous data for debugging
+#'
+#' Generates histograms for any provided continuous variable. When editing
+#' pipelines, these allow the user to check that outputs are as expected. This
+#' functionality will eventually be integrated into the data quality check
+#' pipeline.
+#' @param table A table containing data of interest.
+#' @param variable A character string of the variable name.
+#'
+#' @return A histogram generated with ggplot.
+#' @export
+#' @import ggplot2
+
+plot_debug_hist <- function(table, variable){
+
+  variable <- sym(variable)
+  variable <- enquo(variable)
+  var_name <- snakecase::to_any_case(dplyr::quo_name(variable), case = "sentence")
+
+  #Make a number of bins the same as the number of unique values
+  bins <- table %>%
+    summarise(bins = ifelse(length(unique(!!variable)) < 10, length(unique(!!variable)), length(unique(!!variable))/2))
+
+  #Generate histogram of clutch size
+  raw_dat <- table %>%
+    filter(!is.na(!!variable)) %>%
+    {ggplot(.)+
+        geom_histogram(aes(x = !!variable), bins = bins$bins, fill = "grey", colour = "black")+
+        labs(title = paste0("Histogram of ", tolower(var_name), " from ", pop_names[which(pop_names$code == table$PopID[1]), ]$name),
+             y = "Number of observations", x = var_name)+
+        theme_classic()+
+        theme(plot.title = element_text(hjust = 0.5, size = 20),
+              axis.text = element_text(size = 12, colour = "black"),
+              axis.title = element_text(size = 15, colour = "black"),
+              plot.margin = margin(20, 20, 20, 20))}
+
+  #Facet histograms if there are many species
+  if(length(unique(table$Species)) > 1){
+
+    raw_dat <- raw_dat +
+      facet_wrap(facets = ~Species)
+
+  }
+
+  return(raw_dat)
+
+}
