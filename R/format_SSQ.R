5ecff6aaa6254b4fb2a6176d6e9ab3e1b454e85b
Liam D. Bailey <liam.bailey@liamdbailey.com>
2019-06-17 10:05:18 +0200

    Extract SSQ Brood data

--- R/format_SSQ.R
+++ R/format_SSQ.R
@@ -22,9 +22,9 @@
 #' @import reshape2
 
 format_SSQ <- function(db = NULL,
-                          Species = NULL,
-                          path = ".",
-                          debug = FALSE){
+                       Species = NULL,
+                       path = ".",
+                       debug = FALSE){
 
   #Find database path
   if(is.null(db)){
@@ -39,9 +39,12 @@ format_SSQ <- function(db = NULL,
   start_time <- Sys.time()
 
   #Read in data with readxl
-  all_data <- read_excel(db) %>%
+  all_data <- readxl::read_excel(db) %>%
     #Clean all names with janitor
-    janitor::clean_names(case = "upper_camel")
+    janitor::clean_names(case = "upper_camel") %>%
+    #Remove the column 'Row'. This is just the row number, we have this already.
+    dplyr::select(-Row) %>%
+    janitor::remove_empty(which = "rows")
 
   ##############
   # BROOD DATA #
@@ -56,7 +59,104 @@ format_SSQ <- function(db = NULL,
     mutate(PopID = "SIC",
            BroodID = paste(Year, NestId, sep = "_")) %>%
     left_join(tibble::tibble(ClutchType_observed = c("first", "second", "replacement"),
-                             Class = c(1, 3, 2)), by = "Class")
+                             Class = c(1, 3, 2)), by = "Class") %>%
+    #Change column names to match consistent naming
+    dplyr::rename(SampleYear = Year, LayingDate = Ld, ClutchSize = Cs,
+                  HatchDate = Hd, BroodSize = Hs, NumberFledged = Fs,
+                  FemaleID = FId, MaleID = MId) %>%
+    dplyr::mutate(Species = Code, FledgeDate = NA)
+
+  #Determine ClutchType_calc
+  clutchtype <- dplyr::progress_estimated(n = nrow(Brood_data))
+
+  Brood_data <- Brood_data %>%
+    #Determine the 30 day cut-off for all species
+    group_by(PopID, SampleYear, Species) %>%
+    mutate(cutoff = tryCatch(expr = min(LayingDate, na.rm = T) + 30,
+                             warning = function(...) return(NA))) %>%
+    # Determine brood type for each nest based on female ID
+    arrange(SampleYear, Species, FemaleID) %>%
+    group_by(SampleYear, Species, FemaleID) %>%
+    #Assume NAs in Fledglings are 0s.
+    ### NEED TO CHECK WITH CAMILLO ABOUT THIS
+    mutate(total_fledge = calc_cumfledge(x = NumberFledged, na.rm = T),
+           row = 1:n()) %>%
+    ungroup() %>%
+    mutate(ClutchType_calc = purrr::pmap_chr(.l = list(rows = .$row,
+                                                       femID = .$FemaleID,
+                                                       cutoff_date = .$cutoff,
+                                                       nr_fledge_before = .$total_fledge,
+                                                       LD = .$LayingDate),
+                                             .f = function(rows, femID, cutoff_date, nr_fledge_before, LD){
+
+                                               clutchtype$tick()$print()
+
+                                               #Firstly, check if the nest has a LD
+                                               #If not, we cannot calculate BroodType
+
+                                               if(is.na(LD)){
+
+                                                 return(NA)
+
+                                               }
+
+                                               #Next, check if the female is banded
+                                               #If a female is unbanded we assume the nest can NEVER be secondary
+                                               #If she had had a successful clutch before she would have been caught and banded
+                                               #Therefore it can only be first or replacement (based on 30d rule)
+                                               if(is.na(femID)){
+
+                                                 if(LD > cutoff_date){
+
+                                                   return("replacement")
+
+                                                 } else {
+
+                                                   return("first")
+
+                                                 }
+
+                                               }
+
+                                               #If she is banded, then we need to apply all rules
+                                               #If it's the first nest recorded for this female in this year...
+                                               if(rows == 1){
+
+                                                 #If it doesn't meet the 30 day rule, then name it as replacement
+                                                 if(LD > cutoff_date){
+
+                                                   return("replacement")
+
+                                                 } else {
+
+                                                   #Otherwise, we assume it was the first clutch
+                                                   return("first")
+
+                                                 }
+
+                                                 #If it's NOT the first nest of the season for this female
+                                               } else {
+
+                                                 #If there have been no fledglings before this point..
+                                                 if(nr_fledge_before == 0){
+
+                                                   #Then it is a replacement
+                                                   return("replacement")
+
+                                                 } else {
+
+                                                   #Otherwise, it is a secondary clutch
+                                                   return("second")
+
+                                                 }
+
+                                               }
+
+                                             })) %>%
+    select(SampleYear, Species, PopID, Plot = HabitatOfRinging,
+           LocationID = NestId, BroodID, FemaleID, MaleID,
+           ClutchType_observed, ClutchType_calc,
+           LayingDate:NumberFledged)
 
 
 }
