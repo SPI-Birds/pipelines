0b583a06df6fc77a8e67313f460c877d8af4d41e
Liam D. Bailey <liam.bailey@liamdbailey.com>
2019-06-19 16:55:28 +0200

    Create function to convert tarsus measures

---
+++ R/convert_tarsus.R
@@ -0,0 +1,30 @@
+#' Convert tarsus measurements to Svennson's Alternative
+#'
+#' Convert tarsus lenght measurements in Svennson's Standard and Oxford Maximum
+#' to Svensson's Alternative.
+#'
+#' We consider Svensson's alternative to be the most reliable measurement
+#' method.
+#' @param x A numeric vector. Tarsus length measurements (mm).
+#' @param method Character string. The measurement method of the original data.
+#'   Can be: "Standard" (Svennson's Standard) or "Oxford" (Oxford maximum).
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#'
+#' convert_tarsus(x = c(15, 20, 22), method = "Standard")
+convert_tarsus <- function(x, method){
+
+  if(method == "Standard"){
+
+    return(6.158 + 0.777 * x)
+
+  } else if(method == "Oxford"){
+
+    return(3.64549 + 0.72005 * x)
+
+  }
+
+}
