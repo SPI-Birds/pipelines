7645a528ebb5a380b234e842ed90e3a75a095382
Liam D. Bailey <liam.bailey@liamdbailey.com>
2019-06-19 12:48:07 +0200

    Update UAN brood_data table
    UAN was one of the first pipelines I wrote. Since then, we have added additional columns (e.g. ClutchType_calc). These are now included to follow the standard protocol

--- R/format_UAN.R
+++ R/format_UAN.R
@@ -1,5 +1,18 @@
-#' Construct standard summary for University of Antwerp populations
+#' Construct standard summary for data from the Univeristy of Antwerp.
 #'
+#' A pipeline to produce a standard output for 2 hole-nesting bird study populations
+#' administered by the University of Amsterdam.
+#' Output follows the HNB standard breeding data format.
+#'
+#' This section provides details on data management choices that are unique to the NIOO database.
+#' For a general description of the standard format please see XXXXX PLACE HOLDER!
+#'
+#' \strong{ClutchType_observed}: The raw data distinguishes second and third
+#' nests and first second and third replacements. We group these all as 'second'
+#' and 'replacement'.
+#'
+#' \strong{ExperimentID}: Experimental codes are provided in their original
+#' format. These still need to be translated into our experimental groups.
 #' @param db Directory where raw data files are stored.
 #' @param Species A numeric vector. Which species should be included (EUring codes)? If blank will return all major species (see details below).
 #' @param path Location where output csv files will be saved.
@@ -20,7 +33,7 @@ format_UAN <- function(db = choose.dir(),
 
   start_time <- Sys.time()
 
-  all_files <- list.files(path = data_folder, pattern = ".csv", full.names = TRUE)
+  all_files <- list.files(path = db, pattern = ".csv", full.names = TRUE)
 
   BOX_info <- read.csv(all_files[grepl("BOX", all_files)], header = T, sep = ",", stringsAsFactors = FALSE)
   BROOD_info <- read.csv(all_files[grepl("BR", all_files)], header = T, sep = ",", stringsAsFactors = FALSE)
@@ -35,19 +48,118 @@ format_UAN <- function(db = choose.dir(),
   print("Compiling brood information...")
 
   #Create a table with brood information.
+  clutchtype <- dplyr::progress_estimated(n = nrow(BROOD_info))
 
   Brood_data <- BROOD_info %>%
     #Link the plot codes to the plot code (GB)
-    left_join(select(PLOT_info, PopID = SA, GB = gb), by = "GB") %>%
+    dplyr::left_join(select(PLOT_info, PopID = SA, GB = gb), by = "GB") %>%
     #Convert all other columns into standard format
-    transmute(BroodYear = year,
+    dplyr::mutate(SampleYear = year,
            Species = ifelse(SOORT == "pm", "GT", ifelse(SOORT == "pc", "BT", NA)),
-           PopID = PopID,
-           BroodID = NN, NestboxID = PL,
+           PopID = PopID, Plot = GB,
+           LocationID = PL, BroodID = NN,
            FemaleID = RW, MaleID = RM,
-           ClutchType = ifelse(TY == 1, "First", ifelse(TY > 1, "Second/replacement", NA)),
-           LayingDate = lubridate::dmy(LD),
-           ClutchSize = AE, HatchDate = NA, NumberHatched = NP, FledgeDate = NA, NumberFledged = PU)
+           LayingDate = lubridate::dmy(LD), LayingDateError = NA,
+           ClutchSize = AE, ClutchSizeError = NA,
+           HatchDate = NA, HatchDateError = NA,
+           BroodSize = NP, BroodSizeError = NA,
+           FledgeDate = NA, FledgeDateError = NA,
+           NumberFledged = PU, NumberFledgedError = NA,
+           AvgMass = GG, AvgTarsus = GT,
+           ExperimentID = exp) %>%
+    #Include ClutchType info
+    #Firstly, convert ClutchType_observed
+    dplyr::left_join(tibble::tibble(TY = 0:9,
+                                    ClutchType_observed = c(NA, "First", "Second", "Replacement",
+                                                            "Replacement", "Replacement",
+                                                            "Second", "Second", "Replacement",
+                                                            "First")), by = "TY") %>%
+    #Create ClutchType_calc
+    group_by(PopID, SampleYear, Species) %>%
+    mutate(cutoff = tryCatch(expr = min(LayingDate, na.rm = T) + 30,
+                             warning = function(...) return(NA))) %>%
+    # Determine brood type for each nest based on female ID
+    arrange(SampleYear, Species, FemaleID) %>%
+    group_by(SampleYear, Species, FemaleID) %>%
+    #Assume that any NAs in the Number of fledglings should be 0s.
+    mutate(total_fledge_narm = calc_cumfledge(NumberFledged, na.rm = TRUE),
+           row = 1:n()) %>%
+    ungroup() %>%
+    dplyr::mutate(ClutchType_calc = purrr::pmap_chr(.l = list(rows = .$row,
+                                                              femID = .$FemaleID,
+                                                              cutoff_date = .$cutoff,
+                                                              nr_fledge_before = .$total_fledge_narm,
+                                                              LD = .$LayingDate),
+                                                    .f = function(rows, femID, cutoff_date, nr_fledge_before, LD){
+
+                                                      clutchtype$tick()$print()
+
+                                                      #Firstly, check if the nest has a LD
+                                                      #If not, we cannot calculate BroodType
+
+                                                      if(is.na(LD)){
+
+                                                        return(NA)
+
+                                                      }
+
+                                                      #Next, check if the female is banded
+                                                      #If a female is unbanded we assume the nest can NEVER be secondary
+                                                      #If she had had a successful clutch before she would have been caught and banded
+                                                      #Therefore it can only be first or replacement (based on 30d rule)
+                                                      if(is.na(femID)){
+
+                                                        if(lubridate::ymd(LD) > lubridate::ymd(cutoff_date)){
+
+                                                          return("replacement")
+
+                                                        } else {
+
+                                                          return("first")
+
+                                                        }
+
+                                                      }
+
+                                                      #If she is banded, then we need to apply all rules
+                                                      #If it's the first nest recorded for this female in this year...
+                                                      if(rows == 1){
+
+                                                        #If it doesn't meet the 30 day rule, then name it as replacement
+                                                        if(lubridate::ymd(LD) > lubridate::ymd(cutoff_date)){
+
+                                                          return("replacement")
+
+                                                        } else {
+
+                                                          #Otherwise, we assume it was the first clutch
+                                                          return("first")
+
+                                                        }
+
+                                                        #If it's NOT the first nest of the season for this female
+                                                      } else {
+
+                                                        #If there have been no fledglings before this point..
+                                                        if(nr_fledge_before == 0){
+
+                                                          #Then it is a replacement
+                                                          return("replacement")
+
+                                                        } else {
+
+                                                          #Otherwise, it is a secondary clutch
+                                                          return("second")
+
+                                                        }
+
+                                                      }
+
+                                                    })) %>%
+    dplyr::select(SampleYear, Species, PopID, Plot, LocationID,
+                  BroodID, FemaleID, MaleID, ClutchType_observed,
+                  ClutchType_calc, LayingDate:ExperimentID)
+
 
   ################
   # CAPTURE DATA #
@@ -55,9 +167,9 @@ format_UAN <- function(db = choose.dir(),
 
   print("Compiling capture information...")
 
-  #Capture data includes all times an individual was captured (with measurements like mass, tarsus etc.).
-  #This will include first capture as nestling (for residents)
-  #This means there will be multiple records for a single individual.
+  #Capture data includes all times an individual was captured (with measurements
+  #like mass, tarsus etc.). This will include first capture as nestling (for
+  #residents) This means there will be multiple records for a single individual.
 
   Capture_data <- CAPTURE_info %>%
     tibble::rownames_to_column() %>%
