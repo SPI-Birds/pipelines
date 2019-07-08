819408e82fc0ef6a52be7e3f4b3dc881c68a1a61
Unknown <liam.bailey@liamdbailey.com>
2019-07-08 11:01:20 +0200

    Add additional arg to calc_age for capturedate
    There may be cases where an individual is captured multiple times in one year. We want to sort by both SampleYear and CaptureDate to ensure that we have the first capture of the first season. We don't use just CaptureDate to avoid issue with species that breed over multiple years (e.g. southern hemisphere).

--- R/calc_age.R
+++ R/calc_age.R
@@ -1,27 +1,40 @@
 #' Calculate age based on when an individual was first captured
 #'
-#' Arrange data by individual and year and then determine potential age in each capture.
+#' Arrange data by individual and year and then determine potential age in each
+#' capture.
 #'
-#' We only consider whether an individual was captured as a chick or not.
-#' We don't consider the adult age when an individual was first captured (e.g. EURING 4 or 5)
-#' This prevents any cases where an individual might be wrongly aged at first capture.
-#' @param FirstYr Numeric. When an individual was first captured.
-#' @param CurrentYr Numeric. Current year of capture.
-#' @param FirstAge Numeric. Age of individual when first caught, using EURING codes.
+#' We only consider whether an individual was captured as a chick or not. We
+#' don't consider the adult age when an individual was first captured (e.g.
+#' EURING 4 or 5) This prevents any cases where an individual might be wrongly
+#' aged at first capture.
 #'
-#' @return A numeric vector of EURING codes.
+#' @param data Data frame. Data frame with capture information.
+#' @param ID Unquoted expression (i.e. character without quotation marks). Name
+#'   of column with individual identity.
+#' @param Age Unquoted expression (i.e. character without quotation marks). Name
+#'   of column with observed age of individual in each capture. Must be in
+#'   EURING codes.
+#' @param Date Unquoted expression (i.e. character without quotation marks).
+#'   Name of column with CaptureDate information. Must be in format dd/mm/yyyy.
+#' @param Year Unquoted expression (i.e. character without quotation marks).
+#'   Name of column with year information. N.B. This could be different to
+#'   CaptureDate if we are dealing with species that breed over two year (e.g.
+#'   Southern Hemisphere species).
+#'
+#' @return A data frame with calculated age.
 #' @export
 #'
 #' @examples
 #' set.seed(666)
 #' bird_data <- tibble::tibble(IndvID = LETTERS[sample(1:26, 100, replace = TRUE)],
 #' SampleYear = sample(2012:2016, 100, replace = TRUE),
+#' CaptureDate = as.Date(paste(SampleYear, 3, 31, sep = "-"), format = "%Y-%m-%d") + sample(1:30, 100, replace = TRUE),
 #' Age_obsv = sample(c(1, 4), 100, replace = TRUE)) %>%
-#' calc_age(ID = IndvID, Age = Age_obsv, Year = SampleYear)
-calc_age <- function(data, ID, Age, Year){
+#' calc_age(ID = IndvID, Age = Age_obsv, Date = CaptureDate, Year = SampleYear)
+calc_age <- function(data, ID, Age, Date, Year){
 
   data %>%
-    dplyr::arrange({{ID}}, {{Year}}) %>%
+    dplyr::arrange({{ID}}, {{Year}}, {{Date}}) %>%
     dplyr::group_by({{ID}}) %>%
     dplyr::mutate(FirstAge  = first({{Age}}),
                   FirstYear = first({{Year}})) %>%
