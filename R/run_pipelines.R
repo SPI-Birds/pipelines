f892980a10588612b2fc307141fb6329e172a4e5
Unknown <liam.bailey@liamdbailey.com>
2019-07-31 17:09:56 +0200

    Separate pipelines by species and pop

--- R/run_pipelines.R
+++ R/run_pipelines.R
@@ -31,25 +31,54 @@ run_pipelines <- function(path = utils::choose.dir(),
 
   }
 
-  #Filter pop_names by the specified PopIDs
-  #This tells us the data owner of each population
-  pop_subset <- dplyr::filter(pop_names, code %in% PopID) %>%
-    #For each data owner, create a summary of all the populations
-    #that the user wants to extract.
-    dplyr::group_by(owner) %>%
-    dplyr::summarise(pop = list(c(code)))
+  #Assign species for filtering
+  if(is.null(Species)){
+
+    Species <- Species_codes$Code
+
+  }
+
+  #Firstly, check if there are any cases where a requested population does not have any info on a given species
+  missing_species <- pop_species_combos %>%
+    dplyr::filter(pop %in% PopID) %>%
+    dplyr::group_by(pop) %>%
+    dplyr::summarise(total_sp = sum(species %in% Species))
+
+  #If there are any with missing data, give a warning message
+  if(any(missing_species$total_sp == 0)){
+
+    missing_species %>%
+      dplyr::filter(total_sp == 0) %>%
+      purrr::pwalk(.l = list(.$pop),
+                   .f = ~{
+
+                     message(glue::glue('Population {..1} has no information on the focal species and has been excluded.'))
+
+                   })
+
+  }
+
+  #Now just work with those populations where the population and species are present
+  pop_species_subset <- pop_species_combos %>%
+    dplyr::filter(pop %in% PopID & species %in% Species) %>%
+    dplyr::left_join(dplyr::select(pop_names, code, owner), by = c("pop" = "code")) %>%
+    dplyr::group_by(owner, pop) %>%
+    dplyr::summarise(species = list(c(species))) %>%
+    dplyr::summarise(species = first(species),
+                     pops = list(c(pop)))
 
   #Find the file path for each of the data owners of interest
   all_dirs <- list.dirs(path = path, full.names = TRUE)
-  all_dirs <- all_dirs[grepl(pattern = paste(pop_subset$owner, collapse = "|"), all_dirs)]
+  all_dirs <- all_dirs[grepl(pattern = paste(pop_species_subset$owner, collapse = "|"), all_dirs)]
   all_dirs <- gsub(pattern = "\\", replacement = "/", x = all_dirs, fixed = TRUE)
 
   #For each data owner, run the pipeline using the populations requested
-  purrr::pwalk(.l = list(dirs = all_dirs, owner = pop_subset$owner,
-                         pops = pop_subset$pop),
-               .f = function(dirs, owner, pops){
+  purrr::pwalk(.l = list(dirs = all_dirs, owner = pop_species_subset$owner,
+                         pops = pop_species_subset$pops,
+                         species = pop_species_subset$species),
+               .f = function(dirs, owner, pops, species){
 
-                 eval(parse(text = glue::glue('format_{owner}(db = dirs, pop = pops)')))
+                 eval(parse(text = glue::glue('format_{owner}(db = dirs, pop = pops, species = c(\"{species}\"))')))
 
                })
 
