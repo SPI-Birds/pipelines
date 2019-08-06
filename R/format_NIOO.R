7d7b9f112a501107c12b70897b2e4a674015085b
Unknown <liam.bailey@liamdbailey.com>
2019-08-06 15:01:38 +0200

    Update Capture_data for protocol NIOO

--- R/format_NIOO.R
+++ R/format_NIOO.R
@@ -28,6 +28,19 @@
 #'NumberFledgedError etc. this is the absolute error (+/-) around the best
 #'estimate.
 #'
+#'\strong{CapturePlot, ReleasePlot, LocationID}: NIOO data gives CaptureLocation
+#'and ReleaseLocation (e.g. which nest box was a check transfered to during crossfoster).
+#'Currently, we use these as the Capture/ReleasePlot. In Capture_data
+#'we specify the LocationID as the same as the CapturePlot. We need to fix
+#'this to distinguish between Location (the smallest scale), Plot (middle scale),
+#'and PopID (largest scale).
+#'
+#'\strong{Capture_data}Capture_data has information on the accuracy of capture dates.
+#'For now, I am only including those individuals where capture date is known with
+#'100% accuracy, otherwise this might affect the calculated age of individuals
+#'during capture.
+#'
+#'
 #'@inheritParams pipeline_params
 #'
 #'@return Generates 4 .csv files with data in a standard format.
@@ -139,8 +152,10 @@ format_NIOO <- function(db = utils::choose.dir(),
     dplyr::select(-Sexe) %>%
     dplyr::collect() %>%
     #Add sex from standard protocol
+    #Add a ring age observed for determining Age_observed in Capture_data
     dplyr::mutate(Sex = dplyr::case_when(.$Sex %in% c(1, 3, 5) ~ "F",
-                                         .$Sex %in% c(2, 4, 6) ~ "M")) %>%
+                                         .$Sex %in% c(2, 4, 6) ~ "M"),
+                  RingAgeObsv = RingAge) %>%
     #Add in the first capture location
     #This is needed to determine which population the bird belongs too.
     dplyr::left_join(tbl(connection, "dbo_tbl_Capture") %>%
@@ -168,7 +183,7 @@ format_NIOO <- function(db = utils::choose.dir(),
            BroodIDFledged = purrr::map2_chr(.x = BroodID, .y = GeneticBroodID,
                                            #If there is a genetic broodID listed by no regular brood ID assume these are the same.
                                            .f = ~ifelse(!is.na(.y) & is.na(.x), .y, .x))) %>%
-    dplyr::select(IndvID, RingNumber, Species, PopID, BroodIDLaid, BroodIDFledged, RingSeason, RingAge, Sex) %>%
+    dplyr::select(IndvID, RingNumber, Species, PopID, BroodIDLaid, BroodIDFledged, RingSeason, RingAge, RingAgeObsv, Sex) %>%
     #Convert RingAge into either chick or adult
     dplyr::mutate(RingAge = dplyr::case_when(.$RingAge %in% c(1, 2, 3) ~ "chick",
                                              .$RingAge > 3 ~ "adult"))
@@ -183,12 +198,12 @@ format_NIOO <- function(db = utils::choose.dir(),
   #This will include first capture as nestling
   #This can include multiple records for a single individual.
   Capture_data <- dplyr::tbl(connection, "dbo_tbl_Capture") %>%
-    dplyr::select(CaptureID = ID, CaptureDate, CaptureTime, IndvID = Individual, CaptureLocation, ReleaseLocation, CaptureType) %>%
+    dplyr::select(CaptureID = ID, AccuracyOfDate, CaptureDate, CaptureTime, IndvID = Individual, CaptureLocation, ReleaseLocation, CaptureType) %>%
     #Join in weight, tarsus and wing_length from secondary capture data table.
     dplyr::left_join(dplyr::tbl(connection, "dbo_vw_MI_CaptureCaptureData") %>%
-                       dplyr::select(CaptureID, SpeciesID, Weight, Tarsus, Wing_Length), by = "CaptureID") %>%
+                       dplyr::select(CaptureID, SpeciesID, Observer, Weight, Tarsus, Wing_Length), by = "CaptureID") %>%
     #Filter target species
-    dplyr::filter(SpeciesID %in% species) %>%
+    dplyr::filter(SpeciesID %in% species & AccuracyOfDate == 1) %>%
     #Select only the basic info we need
     # -CaptureID (unique ID of capture event)
     # -CaptureDate
@@ -200,62 +215,75 @@ format_NIOO <- function(db = utils::choose.dir(),
     # -Weight
     # -Tarsus
     # -Wing_Length
-  select(CaptureID, CaptureDate, CaptureTime, IndvID, SpeciesID, CaptureLocation,
-         ReleaseLocation, Weight, Tarsus, WingLength = Wing_Length) %>%
-    collect() %>%
+    # -CaptureType (needed to determine age)
+    dplyr::select(CaptureID, CaptureDate, CaptureTime, IndvID, SpeciesID, CaptureLocation,
+                  ReleaseLocation, Observer, Weight, Tarsus, WingLength = Wing_Length, CaptureType) %>%
+    dplyr::collect() %>%
     #Join in information on when the individual was first ringed (left join from the IndvData)
     #This is used to determine the age of each individual (EUring) at the time of capture
-    left_join(select(Indv_data, IndvID, RingYear, RingAge), by = "IndvID") %>%
+    dplyr::left_join(dplyr::select(Individual_data, IndvID, RingSeason, RingAgeObsv), by = "IndvID") %>%
+    #Convert RingAgeObsv into Age_observed
+    #These numbers should convert exactly, be we need to check them explicitly when I have
+    #wifi again!
+    #Add BreedingSeason
+    dplyr::mutate(Age_observed = RingAgeObsv,
+                  BreedingSeason = lubridate::year(CaptureDate)) %>%
+    calc_age(ID = IndvID, Age = Age_observed, Date = CaptureDate, Year = BreedingSeason, showpb = TRUE) %>%
     #Determine the time between first capture and current capture (in years)
-    mutate(MinAge = lubridate::year(lubridate::ymd(CaptureDate)) - RingYear) %>%
+    # mutate(MinAge = lubridate::year(lubridate::ymd(CaptureDate)) - RingSeason) %>%
     #Adjust this value to account for the age of the bird when first ringed
     #Using EUring codes, so we account for certainty in the age.
-    mutate(MinAge = toupper(as.hexmode(purrr::pmap_dbl(.l = list(.x = RingAge,
-                                                                 .y = MinAge),
-                                                       .f = function(.x, .y){
-
-                                                         #If the age at ringing was unknown make no age estimate
-                                                         if(is.na(.x) | .x == 0){
-
-                                                           return(NA)
-
-                                                         } else {
-
-                                                           #If the individual was in it's first year when ringed
-                                                           if(.x < 4){
-
-                                                             #Use categories where age is certain (5, 7, etc.)
-                                                             return(3 + 2*(.y))
-
-                                                           } else {
-
-                                                             #If it was not caught in first year, use categories where age is uncertain
-                                                             #(6, 8)
-                                                             return(.x + 2*(.y))
-
-                                                           }
-
-                                                         }
-
-                                                       })))) %T>%
+    # mutate(MinAge = toupper(as.hexmode(purrr::pmap_dbl(.l = list(.x = RingAge,
+    #                                                              .y = MinAge),
+    #                                                    .f = function(.x, .y){
+    #
+    #                                                      #If the age at ringing was unknown make no age estimate
+    #                                                      if(is.na(.x) | .x == 0){
+    #
+    #                                                        return(NA)
+    #
+    #                                                      } else {
+    #
+    #                                                        #If the individual was in it's first year when ringed
+    #                                                        if(.x < 4){
+    #
+    #                                                          #Use categories where age is certain (5, 7, etc.)
+    #                                                          return(3 + 2*(.y))
+    #
+    #                                                        } else {
+    #
+    #                                                          #If it was not caught in first year, use categories where age is uncertain
+    #                                                          #(6, 8)
+    #                                                          return(.x + 2*(.y))
+    #
+    #                                                        }
+    #
+    #                                                      }
+    #
+    #                                                    })))) %T>%
     #Include species letter codes for all species
-    {species_pb <<- dplyr::progress_estimated(n = nrow(.))} %>%
-    mutate(Species = purrr::map_chr(.x = .$SpeciesID,
-                                    .f = function(.x){
-
-                                      species_pb$tick()$print()
-
-                                      as.character(Species_codes[which(Species_codes$SpeciesID == .x), "Code"])
-
-                                    }),
-           CapturePlot = NA, ReleasePlot = NA) %>%
+    dplyr::ungroup() %>%
+    dplyr::mutate(Species = dplyr::case_when(.$SpeciesID == 14400 ~ Species_codes[Species_codes$SpeciesID == 14400, ]$Code,
+                                             .$SpeciesID == 14640 ~ Species_codes[Species_codes$SpeciesID == 14640, ]$Code,
+                                             .$SpeciesID == 13490 ~ Species_codes[Species_codes$SpeciesID == 13490, ]$Code,
+                                             .$SpeciesID == 14620 ~ Species_codes[Species_codes$SpeciesID == 14620, ]$Code,
+                                             .$SpeciesID == 14790 ~ Species_codes[Species_codes$SpeciesID == 14790, ]$Code,
+                                             .$SpeciesID == 15980 ~ Species_codes[Species_codes$SpeciesID == 15980, ]$Code,
+                                             .$SpeciesID == 14610 ~ Species_codes[Species_codes$SpeciesID == 14610, ]$Code),
+                  #Add original tarsus method
+                  OriginalTarsusMethod = dplyr::case_when(!is.na(.$Tarsus) ~ "Alternative"),
+                  ChickAge = NA, ObserverID = as.character(Observer)) %>%
     #Arrange by species, indv and date/time
-    arrange(Species, IndvID, CaptureDate, CaptureTime) %>%
+    dplyr::arrange(Species, IndvID, CaptureDate, CaptureTime) %>%
     #Include three letter population codes for both the capture and release location (some individuals may have been translocated e.g. cross-fostering)
-    left_join(dplyr::select(Locations, CaptureLocation = ID, CapturePopID = PopID), by = "CaptureLocation") %>%
-    left_join(dplyr::select(Locations, ReleaseLocation = ID, ReleasePopID = PopID), by = "ReleaseLocation") %>%
+    dplyr::left_join(dplyr::select(Locations, CaptureLocation = ID, CapturePopID = PopID), by = "CaptureLocation") %>%
+    dplyr::left_join(dplyr::select(Locations, ReleaseLocation = ID, ReleasePopID = PopID), by = "ReleaseLocation") %>%
+    dplyr::filter(CapturePopID %in% pop) %>%
+    dplyr::mutate(LocationID = CaptureLocation) %>%
     #Arrange columns
-    select(CaptureID, CaptureDate, CaptureTime, IndvID, Species, CapturePopID, CaptureLocation, ReleasePopID, ReleaseLocation, Mass = Weight, Tarsus, WingLength, MinAge)
+    dplyr::select(IndvID, Species, BreedingSeason, CaptureDate, CaptureTime, ObserverID, LocationID, CapturePopID, CapturePlot = CaptureLocation,
+                  ReleasePopID, ReleasePlot = ReleaseLocation,
+                  Mass = Weight, Tarsus, OriginalTarsusMethod, WingLength, Age_observed, Age_calculated, ChickAge)
 
   ##############
   # BROOD DATA #
@@ -263,7 +291,7 @@ format_NIOO <- function(db = utils::choose.dir(),
 
   #This data will include 1 row for every recorded brood.
 
-  print("Compiling brood information...")
+  message("Compiling brood information...")
 
   Brood_data  <- tbl(connection, "dbo_tbl_Brood") %>%
     #Subset only broods of designated species in main areas
@@ -329,9 +357,9 @@ format_NIOO <- function(db = utils::choose.dir(),
     ########### THIS MEANS THAT WE WILL GET A FEW DUPLICATE RECORDS WITH THIS APPROACH
     ########### THESE NEED TO BE ADDRESSED IN THE DATABASE BEFORE THEY CAN BE FIXED HERE
     #Join in ID numbers for the parents of the brood from the individual table above
-    left_join(select(Indv_data, Female_ring = RingNumber, FemaleID = IndvID) %>%
+    left_join(select(Individual_data, Female_ring = RingNumber, FemaleID = IndvID) %>%
                 filter(Female_ring != ""), by = "Female_ring") %>%
-    left_join(select(Indv_data, Male_ring = RingNumber, MaleID = IndvID) %>%
+    left_join(select(Individual_data, Male_ring = RingNumber, MaleID = IndvID) %>%
                 filter(Male_ring != ""), by = "Male_ring") %>%
     #Join location info (including site ID and nestbox ID)
     left_join(dplyr::select(Locations, BroodLocation = ID, PopID), by = "BroodLocation") %>%
@@ -431,7 +459,9 @@ format_NIOO <- function(db = utils::choose.dir(),
                                              })) %>%
     #Add extra columns where data was not provided
     #N.B. Need to go through and include experiment ID
-    mutate(ClutchSizeError = NA, HatchDateError = NA, FledgeDateError = NA, ExperimentID = NA) %>%
+    mutate(ClutchSizeError = NA, HatchDateError = NA, FledgeDateError = NA, ExperimentID = NA,
+           BroodLocation = as.character(BroodLocation), BroodID = as.character(BroodID),
+           FemaleID = as.character(FemaleID), MaleID = as.character(MaleID)) %>%
     select(SampleYear, Species, PopID, Plot, LocationID = BroodLocation, BroodID, FemaleID, MaleID, ClutchType_observed, ClutchType_calc, LayingDate, LayingDateError,
            ClutchSize, ClutchSizeError, HatchDate, HatchDateError, BroodSize, BroodSizeError, FledgeDate, FledgeDateError, NumberFledged, NumberFledgedError, ExperimentID)
 
@@ -439,7 +469,8 @@ format_NIOO <- function(db = utils::choose.dir(),
   #AT 14-16 DAYS POST HATCHING!!!
   #IF THEY ARE NOT CAUGHT DURING THIS TIME, THEN LEAVE AS NA.
   avg_mass <- Brood_data %>%
-    left_join(left_join(select(Capture_data, CaptureDate, IndvID, Mass, Tarsus), select(Indv_data, IndvID, BroodID = BroodIDRinged), by = "IndvID"), by = "BroodID") %>%
+    left_join(left_join(select(Capture_data, CaptureDate, IndvID, Mass, Tarsus),
+                        dplyr::select(Individual_data, IndvID, BroodID = BroodIDFledged), by = "IndvID"), by = "BroodID") %>%
     #Filter those that were not caught at 14 - 16 days
     mutate(CaptureDate = lubridate::ymd(CaptureDate),
            HatchDate = lubridate::ymd(HatchDate)) %>%
