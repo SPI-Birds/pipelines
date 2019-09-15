1ecc79f6ee6a23ba573dde38395e8732015c76e9
Unknown <liam.bailey@liamdbailey.com>
2019-09-15 11:38:34 +0200

    Update CHO test to use R output
    Using csv output for tests is inefficient because a) we need to save and reload every csv and b) we are testing the output AFTER READING FROM CSV FILE, rather than after the R output (which is a better test of the code). Therefore, we now test on the R output rather than the csv output.

--- tests/testthat/teardown-xyz.R
+++ tests/testthat/teardown-xyz.R
@@ -1,10 +1,11 @@
 #Delete all .csv files in test environment
-purrr::pwalk(.l = list(list.files(pattern = ".csv")),
-             .f = ~{
+# purrr::pwalk(.l = list(list.files(pattern = ".csv")),
+#              .f = ~{
+#
+#                file.remove(eval(..1))
+#
+#              })
 
-               file.remove(eval(..1))
-
-             })
-
-#Remove path object
+#Remove path object and pipeline output object from the Global Env
 rm(path)
+rm(pipeline_output, envir = .GlobalEnv)
--- tests/testthat/test_CHO_pipeline.R
+++ tests/testthat/test_CHO_pipeline.R
@@ -2,60 +2,97 @@ context("Run data quality check on Choupal pipeline output")
 
 test_that("CHO pipeline works...", {
 
-  suppressWarnings(run_pipelines(path = path, PopID = "CHO"))
+  #Save in global environment for use later
+  suppressWarnings(pipeline_output <<- run_pipelines(path = path, PopID = "CHO", output_type = "R"))
 
 })
 
 test_that("CHO outputs all files...", {
 
-  expect_true(file.exists("Brood_data_CHO.csv"))
-  expect_true(file.exists("Capture_data_CHO.csv"))
-  expect_true(file.exists("Individual_data_CHO.csv"))
-  expect_true(file.exists("Location_data_CHO.csv"))
+  expect_true(exists("Brood_data", where = .GlobalEnv$pipeline_output$CHO))
+  expect_true(exists("Capture_data", where = .GlobalEnv$pipeline_output$CHO))
+  expect_true(exists("Individual_data", where = .GlobalEnv$pipeline_output$CHO))
+  expect_true(exists("Location_data", where = .GlobalEnv$pipeline_output$CHO))
 
 })
 
 test_that("CHO individual data has no errors...", {
 
-  check <- check_format_individual(utils::read.csv("Individual_data_CHO.csv", stringsAsFactors = FALSE))
+  check <- check_format_individual(.GlobalEnv$pipeline_output$CHO$Individual_data)
 
   expect_false(check$check_list$Error)
+
+  if(check$check_list$Error == TRUE){
+
+    purrr::pwalk(.l = check$error_output, .f = print)
+
+  }
 
 })
 
 test_that("CHO brood data has no errors...", {
 
   #Check that the format of the data is correct
-  check <- check_format_individual(utils::read.csv("Brood_data_CHO.csv", stringsAsFactors = FALSE))
+  check <- check_format_individual(.GlobalEnv$pipeline_output$CHO$Brood_data)
 
   expect_false(check$check_list$Error)
+
+  if(check$check_list$Error == TRUE){
+
+    purrr::pwalk(.l = check$error_output, .f = print)
+
+  }
 
   #Check that clutch size < brood size
-  check <- compare_clutch_brood(utils::read.csv("Brood_data_CHO.csv", stringsAsFactors = FALSE))
+  check <- compare_clutch_brood(.GlobalEnv$pipeline_output$CHO$Brood_data)
 
   expect_false(check$check_list$Error)
+
+  if(check$check_list$Error == TRUE){
+
+    purrr::pwalk(.l = check$error_output, .f = print)
+
+  }
 
   #Check that brood size < fledglings
-  check <- compare_brood_fledglings(utils::read.csv("Brood_data_CHO.csv", stringsAsFactors = FALSE))
+  check <- compare_brood_fledglings(.GlobalEnv$pipeline_output$CHO$Brood_data)
 
   expect_false(check$check_list$Error)
+
+  if(check$check_list$Error == TRUE){
+
+    purrr::pwalk(.l = check$error_output, .f = print)
+
+  }
 
   #Check that laying date < hatch date
-  check <- compare_laying_hatching(utils::read.csv("Brood_data_CHO.csv", stringsAsFactors = FALSE))
+  check <- compare_laying_hatching(.GlobalEnv$pipeline_output$CHO$Brood_data)
 
   expect_false(check$check_list$Error)
+
+  if(check$check_list$Error == TRUE){
+
+    purrr::pwalk(.l = check$error_output, .f = print)
+
+  }
 
   #Check that hatch date < fledge date
-  check <- compare_hatching_fledging(utils::read.csv("Brood_data_CHO.csv", stringsAsFactors = FALSE))
+  check <- compare_hatching_fledging(.GlobalEnv$pipeline_output$CHO$Brood_data)
 
   expect_false(check$check_list$Error)
+
+  if(check$check_list$Error == TRUE){
+
+    purrr::pwalk(.l = check$error_output, .f = print)
+
+  }
 
 })
 
 test_that("Check for impossible values in CHO brood data...", {
 
   #Check that the format of the data is correct
-  brood_data <- utils::read.csv("Brood_data_CHO.csv", stringsAsFactors = FALSE) %>%
+  brood_data <- .GlobalEnv$pipeline_output$CHO$Brood_data %>%
     split(f = as.factor(.$Species))
 
   purrr::pwalk(.l = list(brood_data),
@@ -64,6 +101,12 @@ test_that("Check for impossible values in CHO brood data...", {
                  check <- check_values_brood(Brood_data = ..1, species = unique(..1$Species))
 
                  expect_false(check$check_list$Error)
+
+                 if(check$check_list$Error == TRUE){
+
+                   purrr::pwalk(.l = check$error_output, .f = print)
+
+                 }
 
                })
 
@@ -89,16 +132,28 @@ test_that("Check for impossible values in CHO brood data...", {
 
 test_that("CHO capture data has no errors...", {
 
-  check <- check_format_individual(utils::read.csv("Capture_data_CHO.csv", stringsAsFactors = FALSE))
+  check <- check_format_individual(.GlobalEnv$pipeline_output$CHO$Capture_data)
 
   expect_false(check$check_list$Error)
+
+  if(check$check_list$Error == TRUE){
+
+    purrr::pwalk(.l = check$error_output, .f = print)
+
+  }
 
 })
 
 test_that("CHO location data has no errors...", {
 
-  check <- check_format_individual(utils::read.csv("Location_data_CHO.csv", stringsAsFactors = FALSE))
+  check <- check_format_individual(.GlobalEnv$pipeline_output$CHO$Location_data)
 
   expect_false(check$check_list$Error)
+
+  if(check$check_list$Error == TRUE){
+
+    purrr::pwalk(.l = check$error_output, .f = print)
+
+  }
 
 })
