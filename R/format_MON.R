247a595fbfe513d8f71d6ba4d50f2eb2b9ad172c
Unknown <liam.bailey@liamdbailey.com>
2019-09-20 14:30:06 +0200

    Read in Chick capture data MON
    Chick and adult captures are in different files. They have both been read it now, but still be standardised.

--- R/format_MON.R
+++ R/format_MON.R
@@ -84,7 +84,7 @@ format_MON <- function(db = utils::choose.dir(),
 #' @examples
 create_capture_MON <- function(db, species_filter){
 
-  Capture_data <- readxl::read_excel(paste0(db, "//", "SIE MORPH 1976-2018.xlsx"),
+  Full_capture_data <- readxl::read_excel(paste0(db, "//", "SIE MORPH 1976-2018.xlsx"),
                                      col_types = c("text")) %>%
     #There are two potential coercion issues in the data
     #1. Numbers are stored as text in the excel sheets. This isn't so problematic, because
@@ -98,8 +98,8 @@ create_capture_MON <- function(db, species_filter){
     dplyr::rename(BreedingSeason = an, CaptureDate = date_mesure, CaptureTime = heure,
                   Species = espece, IndvID = bague, WingLength = aile,
                   TarsusRight = tarsed, TarsusLeft = tarseg, BeakLength = becna,
-                  Weight = poids, ObserverID = obs, LocationID = nic,
-                  Destination_Aviary = Dest) %>%
+                  Mass = poids, ObserverID = obs,
+                  Destination_Aviary = dest, BirthPlot = orig) %>%
     dplyr::mutate_at(.vars = vars(3, 7, 16, 37), as.integer) %>%
     dplyr::mutate_at(.vars = vars(5, 6, 14, 18:24, 26, 27, 35), as.numeric) %>%
     dplyr::mutate(CaptureDate = as.Date(substr(CaptureDate, start = 1, stop = 10), format = "%d/%m/%Y")) %>%
@@ -149,7 +149,81 @@ create_capture_MON <- function(db, species_filter){
 
 
   #Format the Capture data to match the standard protocol
-  #For now I'm just going to subset the
+  #For now I'm just going to work with the data collected around Rouviere and Pirio (Corsica)
+  #These are the areas I know there are long term populations
+  #Will ask Anne about the other populations over Skype
+  #Give them PopID COR (Corsica) and ROU (Rouviere)
+  #Use the codes from within each population as Plot
+  Full_capture_data <- Full_capture_data %>%
+    dplyr::filter(lieu %in% c("cap", "mes", "pir", "tua", "rou")) %>%
+    #Only include capture pop and plot for now, until we work out how to code translocations
+    dplyr::mutate(CapturePopID = dplyr::case_when(.$lieu %in% c("cap", "mes", "pir", "tua") ~ "COR",
+                                           .$lieu == "rou" ~ "ROU"),
+                  CapturePlot = toupper(lieu), ReleasePopID = NA_character_, ReleasePlot = NA_character_,
+                  Tarsus = TarsusRight, OriginalTarsusMethod = dplyr::case_when(!is.na(.$TarsusRight) ~ "Alternative"))
+
+
+
+  #Do the same for the chick capture data
+  #As above, we read all in as text and then coerce afterwards
+  Chick_capture_data <- readxl::read_excel(paste0(db, "//", "SIE POUS 1976-2018.xlsx"),
+                                           col_types = c("text")) %>%
+    dplyr::rename(BreedingSeason = an, CaptureDate = date_mesure, CaptureTime = heure,
+                  Species = espece, IndvID = bague,
+                  TarsusRight = tarsed, TarsusLeft = tarseg,
+                  Mass = poids, ObserverID = obs,
+                  ChickAge = age_plume) %>%
+    dplyr::mutate_at(.vars = vars(2, 15), as.integer) %>%
+    dplyr::mutate_at(.vars = vars(16, 18:20), as.numeric) %>%
+    dplyr::mutate(CaptureDate = as.Date(substr(CaptureDate, start = 1, stop = 10), format = "%d/%m/%Y")) %>%
+    #Add standard species codes
+    dplyr::mutate(Species = dplyr::case_when(.$Species == "ble" ~ Species_codes$Code[which(Species_codes$SpeciesID == 14620)],
+                                             .$Species == "noi" ~ Species_codes$Code[which(Species_codes$SpeciesID == 14610)],
+                                             .$Species == "cha" ~ Species_codes$Code[which(Species_codes$SpeciesID == 14640)],
+                                             .$Species == "eto" ~ "STARLING",
+                                             .$Species == "grp" ~ "UN-IDENTIFIED CREEPER",
+                                             .$Species == "grpj" ~ "SHORT-TOED TREECREEPER",
+                                             .$Species == "grpd" ~ "EURASIAN TREECREEPER",
+                                             .$Species == "hup" ~ "CRESTED TIT",
+                                             .$Species == "sit" ~ Species_codes$Code[which(Species_codes$SpeciesID == 14790)],
+                                             .$Species == "moi" ~ "UN-IDENTIFIED SPARROW",
+                                             .$Species == "moid" ~ "HOUSE SPARROW",
+                                             .$Species == "moif" ~ Species_codes$Code[which(Species_codes$SpeciesID == 15980)],
+                                             .$Species == "non" ~ Species_codes$Code[which(Species_codes$SpeciesID == 14400)])) %>%
+    #Filter by species
+    #Also remove only the pops we know
+    dplyr::filter(Species %in% species_filter & lieu %in% c("cap", "mes", "pir", "tua", "rou")) %>%
+    dplyr::mutate(ActionTaken = dplyr::case_when(.$action == "bcj" ~ "Ringed",
+                                                 .$action == "nb" ~ "Not_Ringed",
+                                                 .$action == "ct" ~ "Caught???"),
+                  ObservedSex = dplyr::case_when(.$sex %in% c(1, 4) ~ "M",
+                                                 .$sex %in% c(2, 5) ~ "F"),
+                  GeneticSex = dplyr::case_when(.$sex_g == 1 ~ "M",
+                                                .$sex_g == 2 ~ "F"),
+                  AgeObserved = 1,
+                  ExperimentID = dplyr::case_when(.$exp_p == "alimente" ~ "Supplemental feeding",
+                                                  .$exp_p == "poussintron" ~ "Behavioural test",
+                                                  .$exp_p == "poussintronfc" ~ "Behavioural test and heart rate",
+                                                  .$exp_p == "transf" ~ "Translocation",
+                                                  .$exp_p == "fc" ~ "Heart rate monitored",
+                                                  .$exp_p == "biopsie" ~ "Underwent biopsy",
+                                                  .$exp_p == "fmr" ~ "Doubly labled water",
+                                                  .$exp_p == "voliere" ~ "Bred in aviary",
+                                                  .$exp_p == "gavage" ~ "Chick force-feeding supplement",
+                                                  .$exp_p == "inuline" ~ "Insulin injection"),
+                  Status = dplyr::case_when(.$etat_sante == "M" ~ "Dead",
+                                            .$etat_sante == "D" ~ "Chick dead",
+                                            .$etat_sante == "E" ~ "Healthy"),
+                  #Make LocationID Plot_BoxNumber
+                  LocationID = paste(lieu, nic, sep = "_"),
+                  #Make CapturePopID and plot. Leave Release NA for now until we work out how to code these
+                  CapturePopID = dplyr::case_when(.$lieu %in% c("cap", "mes", "pir", "tua") ~ "COR",
+                                                  .$lieu == "rou" ~ "ROU"),
+                  CapturePlot = toupper(lieu),
+                  ReleasePopID = NA_character_, ReleasePlot = NA_character_,
+                  Tarsus = TarsusRight, OriginalTarsusMethod = dplyr::case_when(!is.na(.$TarsusRight) ~ "Alternative"),
+                  WingLength = NA_real_)
+
 
 }
 
