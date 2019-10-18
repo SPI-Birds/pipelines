4c8a5f50140ffef1c3bc198629255a8561b981fb
StefanVriend <svriend@gmail.com>
2019-10-18 14:59:18 +0200

    Update reference values with NIOO cutoffs
    Cutoffs used in NIOO's dataset for adult mass, tarsus and chick tarsus are implemented in reference values.

--- R/create_reference_values.R
+++ R/create_reference_values.R
@@ -75,6 +75,20 @@ create_reference_values <- function(db = utils::choose.dir()) {
     POEPAL_BroodSize = tibble::tibble(Reference = c("Warning_min", "Warning_max", "Error_min", "Error_max"),
                                       Value = c(NA, 15, 0, 20)),
     POEPAL_NumberFledged = tibble::tibble(Reference = c("Warning_min", "Warning_max", "Error_min", "Error_max"),
+                                          Value = c(NA, 15, 0, 20)),
+    ## -- Poecile cinctus (Siberian tit)
+    POECIN_ClutchSize = tibble::tibble(Reference = c("Warning_min", "Warning_max", "Error_min", "Error_max"),
+                                       Value = c(NA, 10, 0, 15)),
+    POECIN_BroodSize = tibble::tibble(Reference = c("Warning_min", "Warning_max", "Error_min", "Error_max"),
+                                      Value = c(NA, 15, 0, 20)),
+    POECIN_NumberFledged = tibble::tibble(Reference = c("Warning_min", "Warning_max", "Error_min", "Error_max"),
+                                          Value = c(NA, 15, 0, 20)),
+    ## -- Phoenicurus phoenicurus (Common redstart)
+    PHOPHO_ClutchSize = tibble::tibble(Reference = c("Warning_min", "Warning_max", "Error_min", "Error_max"),
+                                       Value = c(NA, 10, 0, 15)),
+    PHOPHO_BroodSize = tibble::tibble(Reference = c("Warning_min", "Warning_max", "Error_min", "Error_max"),
+                                      Value = c(NA, 15, 0, 20)),
+    PHOPHO_NumberFledged = tibble::tibble(Reference = c("Warning_min", "Warning_max", "Error_min", "Error_max"),
                                           Value = c(NA, 15, 0, 20))
   )
 
@@ -82,82 +96,102 @@ create_reference_values <- function(db = utils::choose.dir()) {
   #based on chick growth curves calculated from Hoge Veluwe
   #See calculate_chick_mass_cutoffs.R for details.
 
+  ## PARMAJ, CYACAE and FICHYP cutoffs for Adult_Mass, Adult_Tarsus and Chick_Tarsus are based on cutoffs from NIOO.
+
   ## - Capture data
   capture_ref_values <- list(
     ## --- Parus major (Great tit)
     PARMAJ_Adult_Mass = tibble::tibble(Reference = c("Warning_min", "Warning_max", "Error_min", "Error_max"),
-                                        Value = c(10.95, 25, 5.5, 50)),
+                                        Value = c(2.5, 25, 0, 50)),
     PARMAJ_Adult_Tarsus = tibble::tibble(Reference = c("Warning_min", "Warning_max", "Error_min", "Error_max"),
-                                          Value = c(13.3, 22, 6.5, 44)),
+                                          Value = c(4, 30, 0, 60)),
     PARMAJ_Chick_Mass  = chick_mass_cutoffs %>%
       dplyr::filter(Species == "PARMAJ") %>%
       dplyr::select(Reference, Value, ChickAge),
     PARMAJ_Chick_Tarsus = tibble::tibble(Reference = c("Warning_min", "Warning_max", "Error_min", "Error_max"),
-                                          Value = c(11.9, 21.3, 5, 40)),
+                                          Value = c(3.5, 30, 0, 60)),
     ## --- Cyanistes caeruleus (Blue tit)
     CYACAE_Adult_Mass = tibble::tibble(Reference = c("Warning_min", "Warning_max", "Error_min", "Error_max"),
-                                        Value = c(9.5, 20.1, 5, 40)),
+                                        Value = c(2.5, 25, 0, 50)),
     CYACAE_Adult_Tarsus = tibble::tibble(Reference = c("Warning_min", "Warning_max", "Error_min", "Error_max"),
-                                          Value = c(10.1, 20.8, 5, 40)),
+                                          Value = c(4, 27, 0, 54)),
     CYACAE_Chick_Mass = chick_mass_cutoffs %>%
       dplyr::filter(Species == "CYACAE") %>%
       dplyr::select(Reference, Value, ChickAge),
     CYACAE_Chick_Tarsus = tibble::tibble(Reference = c("Warning_min", "Warning_max", "Error_min", "Error_max"),
-                                          Value = c(14.6, 26, 7, 52)),
+                                          Value = c(3, 27, 0, 54)),
     ## --- Ficedula hypoleuca (Pied flycatcher)
     FICHYP_Adult_Mass = tibble::tibble(Reference = c("Warning_min", "Warning_max", "Error_min", "Error_max"),
-                                        Value = c(10, 20, 5, 25)),
+                                        Value = c(3.5, 25, 0, 50)),
     FICHYP_Adult_Tarsus = tibble::tibble(Reference = c("Warning_min", "Warning_max", "Error_min", "Error_max"),
-                                          Value = c(15, 22, 12, 25)),
+                                          Value = c(5, 23, 0, 46)),
     FICHYP_Chick_Mass = tibble::tibble(Reference = c("Warning_min", "Warning_max", "Error_min", "Error_max"),
-                                        Value = c(10, 20, 5, 25)),
+                                        Value = c(2, 25, 0, 50)),
     FICHYP_Chick_Tarsus = tibble::tibble(Reference = c("Warning_min", "Warning_max", "Error_min", "Error_max"),
-                                          Value = c(15, 22, 12, 25)),
+                                          Value = c(3, 23, 0, 46)),
     ## --- Sitta europaea (Eurasian nutnatch)
     SITEUR_Adult_Mass = tibble::tibble(Reference = c("Warning_min", "Warning_max", "Error_min", "Error_max"),
-                                        Value = c(10, 20, 5, 25)),
+                                        Value = c(3.5, 25, 0, 50)),
     SITEUR_Adult_Tarsus = tibble::tibble(Reference = c("Warning_min", "Warning_max", "Error_min", "Error_max"),
-                                          Value = c(15, 22, 12, 25)),
+                                          Value = c(5, 23, 0, 46)),
     SITEUR_Chick_Mass = tibble::tibble(Reference = c("Warning_min", "Warning_max", "Error_min", "Error_max"),
-                                        Value = c(10, 20, 5, 25)),
+                                        Value = c(2, 25, 0, 50)),
     SITEUR_Chick_Tarsus = tibble::tibble(Reference = c("Warning_min", "Warning_max", "Error_min", "Error_max"),
-                                          Value = c(15, 22, 12, 25)),
+                                          Value = c(3, 23, 0, 46)),
     ## --- Periparus ater (Coal tit)
     PERATE_Adult_Mass = tibble::tibble(Reference = c("Warning_min", "Warning_max", "Error_min", "Error_max"),
-                                        Value = c(10, 20, 5, 25)),
+                                        Value = c(3.5, 25, 0, 50)),
     PERATE_Adult_Tarsus = tibble::tibble(Reference = c("Warning_min", "Warning_max", "Error_min", "Error_max"),
-                                          Value = c(15, 22, 12, 25)),
+                                          Value = c(5, 23, 0, 46)),
     PERATE_Chick_Mass = tibble::tibble(Reference = c("Warning_min", "Warning_max", "Error_min", "Error_max"),
-                                        Value = c(10, 20, 5, 25)),
+                                        Value = c(2, 25, 0, 50)),
     PERATE_Chick_Tarsus = tibble::tibble(Reference = c("Warning_min", "Warning_max", "Error_min", "Error_max"),
-                                          Value = c(15, 22, 12, 25)),
+                                          Value = cc(3, 23, 0, 46)),
     ## --- Passer montanus (Eurasian tree sparrow)
     PASMON_Adult_Mass = tibble::tibble(Reference = c("Warning_min", "Warning_max", "Error_min", "Error_max"),
-                                        Value = c(10, 20, 5, 25)),
+                                        Value = c(3.5, 25, 0, 50)),
     PASMON_Adult_Tarsus = tibble::tibble(Reference = c("Warning_min", "Warning_max", "Error_min", "Error_max"),
-                                          Value = c(15, 22, 12, 25)),
+                                          Value = c(5, 23, 0, 46)),
     PASMON_Chick_Mass = tibble::tibble(Reference = c("Warning_min", "Warning_max", "Error_min", "Error_max"),
-                                        Value = c(10, 20, 5, 25)),
+                                        Value = c(2, 25, 0, 50)),
     PASMON_Chick_Tarsus = tibble::tibble(Reference = c("Warning_min", "Warning_max", "Error_min", "Error_max"),
-                                          Value = c(15, 22, 12, 25)),
+                                          Value = c(3, 23, 0, 46)),
     ## --- Ficedula albicollis (Collared flycatcher)
     FICALB_Adult_Mass = tibble::tibble(Reference = c("Warning_min", "Warning_max", "Error_min", "Error_max"),
-                                        Value = c(10, 20, 5, 25)),
+                                        Value = c(3.5, 25, 0, 50)),
     FICALB_Adult_Tarsus = tibble::tibble(Reference = c("Warning_min", "Warning_max", "Error_min", "Error_max"),
-                                          Value = c(15, 22, 12, 25)),
+                                          Value = c(5, 23, 0, 46)),
     FICALB_Chick_Mass = tibble::tibble(Reference = c("Warning_min", "Warning_max", "Error_min", "Error_max"),
-                                        Value = c(10, 20, 5, 25)),
+                                        Value = c(2, 25, 0, 50)),
     FICALB_Chick_Tarsus = tibble::tibble(Reference = c("Warning_min", "Warning_max", "Error_min", "Error_max"),
-                                          Value = c(15, 22, 12, 25)),
+                                          Value = c(3, 23, 0, 46)),
     ## --- Poecile palustris (Marsh tit)
     POEPAL_Adult_Mass = tibble::tibble(Reference = c("Warning_min", "Warning_max", "Error_min", "Error_max"),
-                                        Value = c(10, 20, 5, 25)),
+                                        Value = c(3.5, 25, 0, 50)),
     POEPAL_Adult_Tarsus = tibble::tibble(Reference = c("Warning_min", "Warning_max", "Error_min", "Error_max"),
-                                          Value = c(15, 22, 12, 25)),
+                                          Value = c(5, 23, 0, 46)),
     POEPAL_Chick_Mass = tibble::tibble(Reference = c("Warning_min", "Warning_max", "Error_min", "Error_max"),
-                                        Value = c(10, 20, 5, 25)),
+                                        Value = c(2, 25, 0, 50)),
     POEPAL_Chick_Tarsus = tibble::tibble(Reference = c("Warning_min", "Warning_max", "Error_min", "Error_max"),
-                                          Value = c(15, 22, 12, 25))
+                                          Value = c(3, 23, 0, 46)),
+    ## --- Poecile cinctus (Siberian tit)
+    POECIN_Adult_Mass = tibble::tibble(Reference = c("Warning_min", "Warning_max", "Error_min", "Error_max"),
+                                       Value = c(3.5, 25, 0, 50)),
+    POECIN_Adult_Tarsus = tibble::tibble(Reference = c("Warning_min", "Warning_max", "Error_min", "Error_max"),
+                                         Value = c(5, 23, 0, 46)),
+    POECIN_Chick_Mass = tibble::tibble(Reference = c("Warning_min", "Warning_max", "Error_min", "Error_max"),
+                                       Value = c(2, 25, 0, 50)),
+    POECIN_Chick_Tarsus = tibble::tibble(Reference = c("Warning_min", "Warning_max", "Error_min", "Error_max"),
+                                         Value = c(3, 23, 0, 46)),
+    ## --- Phoenicurus phoenicurus (Common redstart)
+    PHOPHO_Adult_Mass = tibble::tibble(Reference = c("Warning_min", "Warning_max", "Error_min", "Error_max"),
+                                       Value = c(3.5, 25, 0, 50)),
+    PHOPHO_Adult_Tarsus = tibble::tibble(Reference = c("Warning_min", "Warning_max", "Error_min", "Error_max"),
+                                         Value = c(5, 23, 0, 46)),
+    PHOPHO_Chick_Mass = tibble::tibble(Reference = c("Warning_min", "Warning_max", "Error_min", "Error_max"),
+                                       Value = c(2, 25, 0, 50)),
+    PHOPHO_Chick_Tarsus = tibble::tibble(Reference = c("Warning_min", "Warning_max", "Error_min", "Error_max"),
+                                         Value = c(3, 23, 0, 46)),
   )
 
   ## Save as .rda
