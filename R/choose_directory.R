fbd313a7b97e1211af37de8f4f8de7c7fb697467
Liam D. Bailey <liam.bailey@liamdbailey.com>
2020-02-23 16:44:06 +0100

    Create manual choose_dir that is OS agnostic
    On both Mac and OS, the default is to use selectDirectory from rstudioapi. If Rstudio is not installed, choose.dir() will be used as an alternative. Otherwise, on Mac OSX, an error will be returned. In this case, manual pathname specification will be required.

---
+++ R/choose_directory.R
@@ -0,0 +1,26 @@
+#' Choose folder interactively.
+#'
+#' On Windows, will use 'choose.dir' on Mac will use 'tk_choose.dir'.
+#'
+#' @return A pathname.
+#' @export
+#'
+
+choose_directory <- function() {
+  #As long as users have RstudioAPI available, use the selectDirectory function
+  if(rstudioapi::hasFun("selectDirectory")){
+
+    return(rstudioapi::selectDirectory())
+
+  #Otherwise, check if choose.dir is available (i.e. Windows) and use this instead
+  } else if(rstudioapi::hasFun("choose.dir")) {
+
+    choose.dir(caption = 'Select data directory')
+
+  } else {
+
+    stop("Please run this pipeline in RStudio or manually specify the directory.")
+
+  }
+
+}
