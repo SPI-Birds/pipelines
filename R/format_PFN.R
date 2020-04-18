0490f41111e0bae0765f6d44129a726bc4a8cfef
Chloé R. Nater <chloe_nater@hotmail.com>
2020-04-18 12:01:34 +0200

    Update to PFN pipeline
    Added functionality to determine StartSeason and EndSeason forLocation data based on Brood data. Assuming LocationID = NestboxID, and StartSeason/EndSeason referring to nestbox use by breeding birds, not data collectors. 
    
    Fixed NA formatting.

--- R/format_PFN.R
+++ R/format_PFN.R
@@ -52,17 +52,17 @@ format_PFN <- function(db,
 
   Location_data <- create_location_PFN(Brood_data = Brood_data)
 
-  
+
   # WRANGLE DATA FOR EXPORT
 
   # Capture data: Merge in information on ChickAge (from Brood_data) & remove unnecessary columns
   ChickAge_data <- Brood_data[,c('BroodID', 'ChickAge')]
-  Capture_data <- merge(Capture_data, ChickAge_data, by = 'BroodID', all.x = T, all.y = F)
+  Capture_data <- dplyr::left_join(Capture_data, ChickAge_data, by = 'BroodID')
   Capture_data$ChickAge[which(Capture_data$Age_observed > 1)] <- NA_integer_
-  
+
   Capture_data <- Capture_data %>%
     dplyr::select(-BroodID, -Sex)
-  
+
   # Brood data: Remove unnecessary columns
   Brood_data <- Brood_data %>%
     dplyr::select(-ChickAge)
@@ -111,13 +111,6 @@ format_PFN <- function(db,
 #'
 #' @return A data frame with Brood data
 
-# 0) Defining helper function
-mean_countObs <- function(x){
-  mean <- mean(as.numeric(x), na.rm = T)
-  noObs <- length(which(!is.na(x)))
-  return(c(mean, noObs))
-}
-
 
 create_brood_PFN <- function(db){
 
@@ -148,32 +141,32 @@ create_brood_PFN <- function(db){
                   ClutchType_observed = dplyr::case_when(CltCd == "1" ~ "first",
                                                         CltCd == "2" ~ "replacement",
                                                         CltCd == "3" ~ "second"), # Needs double-checking with data owner
-                  LayDate = as.Date(paste('31/03/', BreedingSeason, sep = ''), format = "%d/%m/%Y") + as.numeric(DFE), 
+                  LayDate = as.Date(paste('31/03/', BreedingSeason, sep = ''), format = "%d/%m/%Y") + as.numeric(DFE),
                   # NOTE: The date calculations require knowing from which date onwards DFE and DH are determined.
                   #       In Smith et al. (2011), a paper mentioned on PiedFly.Net and of which Malcolm is a co-author, I found that 61 corresponds to 31. May. This would indicate that the "baseline" is 31. March, with DFE or DH = 1 corresponding to 1. April
                   #       This NEEDS TO BE VERIFIED by the data owner!
                   #
                   #       REFERENCE:
                   #       Smith, Ken W., et al. "Large-scale variation in the temporal patterns of the frass fall of defoliating caterpillars in oak woodlands in Britain: implications for nesting woodland birds." Bird Study 58.4 (2011): 506-511.
-                 
+
                   ClutchSize = as.integer(CltSize),
                   HatchDate = as.Date(paste('31/03/', BreedingSeason, sep = ''), format = "%d/%m/%Y") + as.numeric(DH), # See note on LayingDate
                   BroodSize = as.integer(Hatch),
                   NumberFledged = as.integer(Fledged),
                   YoungDate = as.Date(YoungDate, format = "%d/%m/%Y")) %>%
-  # NOTE: YoungDate is not part of the standard format, but we need it here to determine chick age, and therefore whether or not we can use the chick measurements) 
-    
+  # NOTE: YoungDate is not part of the standard format, but we need it here to determine chick age, and therefore whether or not we can use the chick measurements)
+
       # 4) Add columns that are based on calculations
       dplyr::arrange(BreedingSeason, FemaleID, LayDate)
       # --> This sorting is required for the function "calc_clutchtype" to work
-    
+
     # 4.1) Calculate means and observation numbers for fledgling weight/tarsus length
     Weight_data <- Brood_data[,paste("Weight.Y", c(1:11), sep = "")]
     Tarsus_data <- Brood_data[,paste("Tarsus.Y", c(1:11), sep = "")]
-  
+
     Weight_data_sum <- do.call("rbind", sapply(1:nrow(Weight_data), FUN = function(x)  mean_countObs(Weight_data[x,]), simplify = FALSE))
     Tarsus_data_sum <- do.call("rbind", sapply(1:nrow(Tarsus_data), FUN = function(x)  mean_countObs(Tarsus_data[x,]), simplify = FALSE))
-  
+
     # 4.2) Add calculated columns
     Brood_data <- Brood_data %>%
       dplyr::mutate(ClutchType_calculated = calc_clutchtype(data = Brood_data, na.rm = FALSE),
@@ -202,7 +195,7 @@ create_brood_PFN <- function(db){
                     AvgEggMass =  NA_real_,
                     OriginalTarsusMethod = NA_character_,
                     ExperimentID = NA_character_) %>%
-    
+
       # 6) Select and arrange columns for output
       dplyr::select(BroodID, PopID, BreedingSeason,
                     Species, Plot, LocationID, FemaleID, MaleID,
@@ -218,9 +211,9 @@ create_brood_PFN <- function(db){
                     AvgTarsus, NumberChicksTarsus,
                     OriginalTarsusMethod,
                     ExperimentID, ChickAge)
-  
+
   # Return data
-  return(Brood_data)
+  return(tibble::as_tibble(Brood_data))
 
 }
 
@@ -234,9 +227,9 @@ create_capture_PFN <- function(db){
 
   Capture_data <- read.csv(file = paste0(db, "/PFN_PrimaryData.csv"), na.strings = c("", "?"),
                            colClasses = "character")
-  
+
   # 1) Male capture data
-  Male_Capture_data <- data.frame(IndvID = Capture_data$MaleID, 
+  Male_Capture_data <- data.frame(IndvID = Capture_data$MaleID,
                                   BroodID = Capture_data$BroodID, # Will be removed later
                                   Species = dplyr::case_when(Capture_data$Species == "BLUTI" ~ "CYACAE",
                                                              Capture_data$Species == "PIEFL" ~ "FICHYP",
@@ -246,7 +239,7 @@ create_capture_PFN <- function(db){
                                                              Capture_data$Species == "NUTHA" ~ "SITEUR",
                                                              Capture_data$Species == "COATI" ~ "PERATE",
                                                              Capture_data$Species == "WREN" ~ "NA",
-                                                             Capture_data$Species == "TREEC" ~ "NA"), 
+                                                             Capture_data$Species == "TREEC" ~ "NA"),
                                   BreedingSeason = as.numeric(Capture_data$Year),
                                   CaptureDate = as.Date(Capture_data$MaleDate, format = "%d/%m/%Y"),
                                   CaptureTime = NA,
@@ -264,10 +257,10 @@ create_capture_PFN <- function(db){
                                   Age_calculated = NA_integer_, # Will be added later
                                   #ChickAge = NA_integer_, # Will be added later
                                   Sex = "M", # Will be removed later
-                                  stringsAsFactors = FALSE) 
-  
+                                  stringsAsFactors = FALSE)
+
   # 2) Female capture data
-  Female_Capture_data <- data.frame(IndvID = Capture_data$FemaleID, 
+  Female_Capture_data <- data.frame(IndvID = Capture_data$FemaleID,
                                     BroodID = Capture_data$BroodID, # Will be removed later
                                     Species = dplyr::case_when(Capture_data$Species == "BLUTI" ~ "CYACAE",
                                                                Capture_data$Species == "PIEFL" ~ "FICHYP",
@@ -277,7 +270,7 @@ create_capture_PFN <- function(db){
                                                                Capture_data$Species == "NUTHA" ~ "SITEUR",
                                                                Capture_data$Species == "COATI" ~ "PERATE",
                                                                Capture_data$Species == "WREN" ~ "NA",
-                                                               Capture_data$Species == "TREEC" ~ "NA"), 
+                                                               Capture_data$Species == "TREEC" ~ "NA"),
                                     BreedingSeason = as.numeric(Capture_data$Year),
                                     CaptureDate = as.Date(Capture_data$FemaleDate, format = "%d/%m/%Y"),
                                     CaptureTime = NA,
@@ -296,11 +289,11 @@ create_capture_PFN <- function(db){
                                     #ChickAge = NA_integer_, # Will be added later
                                     Sex = "F", # Will be removed later
                                     stringsAsFactors = FALSE)
-  
+
   # 3) Chick capture data
   Chick_Capture_list <- list()
   for(i in 1:nrow(Capture_data)){
-    Chick_Capture_list[[i]] <- data.frame(IndvID = unname(t(Capture_data[i, paste0("Young", c(1:11))])), 
+    Chick_Capture_list[[i]] <- data.frame(IndvID = unname(t(Capture_data[i, paste0("Young", c(1:11))])),
                                           BroodID = Capture_data$BroodID[i], # Will be removed later
                                           Species = dplyr::case_when(Capture_data$Species[i] == "BLUTI" ~ "CYACAE",
                                                                      Capture_data$Species[i] == "PIEFL" ~ "FICHYP",
@@ -310,10 +303,10 @@ create_capture_PFN <- function(db){
                                                                      Capture_data$Species[i] == "NUTHA" ~ "SITEUR",
                                                                      Capture_data$Species[i] == "COATI" ~ "PERATE",
                                                                      Capture_data$Species[i] == "WREN" ~ "NA",
-                                                                     Capture_data$Species[i] == "TREEC" ~ "NA"), 
+                                                                     Capture_data$Species[i] == "TREEC" ~ "NA"),
                                           BreedingSeason = as.numeric(Capture_data$Year[i]),
                                           CaptureDate = as.Date(Capture_data$FemaleDate[i], format = "%d/%m/%Y"),
-                                          CaptureTime = NA,
+                                          CaptureTime = strptime(NA, format = "%hh:%mm"),
                                           ObserverID = NA_character_,
                                           LocationID = Capture_data$Box[i],
                                           CapturePopID = "PFN",
@@ -328,27 +321,27 @@ create_capture_PFN <- function(db){
                                           Age_calculated = NA_integer_, # Will be added later
                                           #ChickAge = NA_integer_, # Will be added later
                                           Sex = NA_character_, # Will be removed later
-                                          stringsAsFactors = FALSE) 
+                                          stringsAsFactors = FALSE)
   }
-  
+
   Chick_Capture_data <- dplyr::bind_rows(Chick_Capture_list)
-  
-  
+
+
   # 4) Combine data and remove missing IDs
-  
+
   Capture_data <- dplyr::bind_rows(Chick_Capture_data, Male_Capture_data, Female_Capture_data)
-  
+
   Capture_data <- subset(Capture_data, !is.na(IndvID))
-  
-  
-  # 5) Calculate age at capture 
+
+
+  # 5) Calculate age at capture
 
   # Sort chronologically within individual
   Capture_data <- Capture_data %>%
     dplyr::arrange(IndvID, CaptureDate, CaptureTime) %>%
     #Calculate age at each capture based on first capture
     calc_age(ID = IndvID, Age = Age_observed, Date = CaptureDate, Year = BreedingSeason)
-    
+
   # 6) Return data
   return(Capture_data)
 
@@ -361,57 +354,57 @@ create_capture_PFN <- function(db){
 #' @return A data frame with Individual data
 
 create_individual_PFN <- function(Capture_data){
-  
+
   #Take capture data and determine summary data for each individual
   Indv_data <- Capture_data %>%
     dplyr::filter(!is.na(IndvID)) %>%
     dplyr::arrange(IndvID, BreedingSeason, CaptureDate, CaptureTime) %>%
     dplyr::group_by(IndvID) %>%
-    
+
     #* CN: Loop over the species assigned to each individual and check the number of different assignments
-    
+
     dplyr::summarise(Species = purrr::map_chr(.x = list(unique(na.omit(Species))), .f = ~{
-      
+
       if(length(..1) == 0){
-        
+
         return(NA_character_)
-        
+
       } else if(length(..1) == 1){
-        
+
         return(..1)
-        
+
       } else {
-        
+
         return("CONFLICTED")
-        
+
       }
-      
+
     }), PopID = "PFN",
     BroodIDLaid = first(BroodID),
     BroodIDFledged = BroodIDLaid, #* CN: This would have to be different if there were cross-fostering experiments
     RingSeason = first(BreedingSeason),
     RingAge = ifelse(any(Age_calculated %in% c(1, 3)), "chick", ifelse(min(Age_calculated) == 2, NA_character_, "adult")),
     Sex = purrr::map_chr(.x = list(unique(na.omit(Sex))), .f = ~{
-      
+
       if(length(..1) == 0){
-        
+
         return(NA_character_)
-        
+
       } else if(length(..1) == 1){
-        
+
         return(..1)
-        
+
       } else {
-        
+
         return("C") #* CN: Only one letter used here to keep it at the same length as sex
-        
+
       }
-      
+
     })) %>%
     dplyr::rowwise() %>%
-    
+
     #* CN: Another way to do a for loop
-    
+
     #For each individual, if their ring age was 1 or 3 (caught in first breeding year)
     #Then we take their first BroodID, otherwise it is NA
     dplyr::mutate(BroodIDLaid = ifelse(RingAge == "chick", BroodIDLaid, NA),
@@ -419,9 +412,9 @@ create_individual_PFN <- function(Capture_data){
     #Ungroup to prevent warnings in debug report
     dplyr::ungroup() %>%
     dplyr::arrange(RingSeason, IndvID)
-  
+
   return(Indv_data)
-  
+
 }
 
 #' Create location data table for EastDartmoor.
@@ -434,23 +427,54 @@ create_location_PFN <- function(Brood_data){
 
   # 1) Start a dataframe with all unique LocationIDs / NestboxIDs
   # NOTE: In this case, they are identical. Otherwise, this would have to be all unique combinations of LocationID and NestboxID
-  
-  Location_data <- tibble(LocationID = unique(Brood_data$LocationID), 
+
+  Location_data <- tibble(LocationID = unique(Brood_data$LocationID),
                               NestboxID = unique(Brood_data$LocationID)) %>%
-    
+
     # 2) Add additional columns
     dplyr::mutate(LocationType = 'NB',
                   PopID = 'PFN',
                   Latitude = NA_real_,
                   Longitude = NA_real_,
-                  StartSeason = NA_real_,
-                  EndSeason = NA_real_,
-                  Habitat = NA_real_)
+                  Habitat = NA_real_) %>%
+
+     # 3) Merge information on nestbox usage by birds
+    dplyr::left_join(calc_birdNBuse(Brood_data = Brood_data), by = "LocationID") %>%
+
+    # 4) Select relevant columns
+    dplyr::select(LocationID, NestboxID,
+                  LocationType, PopID,
+                  Latitude, Longitude,
+                  StartSeason, EndSeason,
+                  Habitat)
 
   return(Location_data)
 
 }
 
+
+
+# HELPER FUNCTIONS
+#------------------
+
+#' Return mean and number of observations of a data vector
+#'
+#' @param x A single row of relevant columns in a data frame
+#'
+#' @return A vector containing a calculated mean and a number of observations
+
+mean_countObs <- function(x){
+
+  mean <- mean(as.numeric(x), na.rm = T)
+
+  if(is.nan(mean)){
+    mean <- NA_real_
+  }
+
+  noObs <- length(which(!is.na(x)))
+
+  return(c(mean, noObs))
+}
 
 
 #---------------------------------------
