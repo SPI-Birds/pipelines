002dc7bb32445d6ca327cadc8a05a4dd120263db
Stefan <svriend@gmail.com>
2020-05-22 13:45:42 +0200

    Create whitelist data object
    "whitelist" is a list consisting of 4 data frames (i.e., Brood_whitelist, Capture_whitelist, Individual_whitelist and Location_whitelist), each of which consist of 3 variables: PopID, unique record ID (i.e., BroodID, CaptureID, IndvID and LocationID), and CheckID. This whitelist object will be called in each single quality check function and anti-joined with the records that are flagged as warnings and errors.
    The R data object "whitelist" will be available in the data folder of the package. It will be manually created here in the script whitelist.R, simply by adding rows.

---
+++ R/whitelist.R
@@ -0,0 +1,58 @@
+#' Create whitelist
+#'
+#' A function to create a whitelist of values that are verified as correct by data owners but previously marked as warning/error.
+#'
+#' When a data owner is sent a quality check report, they can determine whether the flagged records are a) unverifiable, b) verifiable to be inccorect, or c) verifiable to be correct. If a), it cannot be known for certain whether the information should be kept or removed. They are continued to be flagged as improbable/impossible, and it is up to the user to decide how they treat them in analysis. If b), the data owner is expected to fix this in the raw data, and it will automatically be no longer flagged in future reports. If c), the records will be whitelisted for future quality checks.
+#'
+#' To whitelist records, there are to pieces of information we use: the unique identifier of the record (e.g., BroodID in Brood_data) and the unique identifier of the quality check that resulted in the original warning/error (e.g., B2).
+#'
+#' @param db File path. Location to save whitelist. Default pipelines/data folder.
+#'
+#' @return
+#' List of 4 dataframes:
+#' \item{Brood_whitelist}{Whitelisted records in Brood_data.}
+#' \item{Capture_whitelist}{Whitelisted records in Capture_data.}
+#' \item{Individual_whitelist}{Whitelisted records in Individual_data.}
+#' \item{Location_whitelist}{Whitelisted records in Location_data.}
+#'
+#' @export
+
+create_whitelist <- function(db = paste0(getwd(), "/data/")){
+
+  # Whitelist for Brood_data
+  Brood_whitelist <- tibble::tibble(PopID = NA_character_,
+                                    BroodID = NA_character_,
+                                    CheckID = NA_character_)
+
+  # First entry
+  # Brood_whitelist <- Brood_whitelist %>%
+  #   dplyr::mutate(PopID = "", BroodID = "", CheckID = "")
+
+  # Second and higher entries
+  # Brood_whitelist <- Brood_whitelist %>%
+  #   tibble::add_row(PopID = "", BroodID = "", CheckID = "")
+
+  # Whitelist for Capture_data
+  Capture_whitelist <- tibble::tibble(PopID = NA_character_,
+                                      CaptureID = NA_character_,
+                                      CheckID = NA_character_)
+
+  # Whitelist for Individual_data
+  Individual_whitelist <- tibble::tibble(PopID = NA_character_,
+                                         IndvID = NA_character_,
+                                         CheckID = NA_character_)
+
+  # Whitelist for Location_data
+  Location_whitelist <- tibble::tibble(PopID = NA_character_,
+                                       LocationID = NA_character_,
+                                       CheckID = NA_character_)
+
+  # Combine data-specific whitelists into one list object
+  whitelist <- list(Brood_whitelist = Brood_whitelist,
+                    Capture_whitelist = Capture_whitelist,
+                    Individual_whitelist = Individual_whitelist,
+                    LocatioN_whitelist = Location_whitelist)
+
+  save(whitelist,
+       file = paste0(db, "whitelist.rda"))
+}
