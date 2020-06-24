37ca4c7fae763207f676a0e890f09c2df42cd908
Stefan <svriend@gmail.com>
2020-06-24 16:13:59 +0200

    Restructure dummy data for testing
    Dummy data is restructured to test quality checks' functionality. Dummy data now ensures that correct records should not be flagged, violated records MUST be flagged.

--- R/approved_list.R
+++ R/approved_list.R
@@ -2,11 +2,11 @@
 #'
 #' A function to create a list of values that are verified as correct by data owners but previously marked as warning/error.
 #'
-#' When a data owner is sent a quality check report, they can determine whether the flagged records are a) unverifiable, b) verifiable and inccorect, or c) verifiable and correct. If a), it cannot be known for certain whether the information should be kept or removed. They are continued to be flagged as improbable/impossible, and it is up to the user to decide how they treat them in analysis. If b), it will continue to be flagged until the data owner changes the value in the raw data. If c), the records will be disregarded for future quality checks.
+#' When a data owner is sent a quality check report, they can determine whether the flagged records are a) unverifiable, b) verifiable and incorrect, or c) verifiable and correct. If a), it cannot be known for certain whether the information should be kept or removed. They are continued to be flagged as improbable/impossible, and it is up to the user to decide how they treat them in analysis. If b), it will continue to be flagged until the data owner changes the value in the raw data. If c), the records will be disregarded for future quality checks.
 #'
 #' To prevent verified records from flagged in future quality checks, there are two pieces of information we use: the unique identifier of the record (e.g., BroodID in Brood_data) and the unique identifier of the quality check that resulted in the original warning/error (e.g., B2).
 #'
-#' @param new_approved_list A list of the same format as the approved_list with new approved records.
+#' @param new_approved_list A list of the same format as the approved_list with new verified records.
 #'
 #' @return
 #' List of 4 dataframes:
@@ -27,10 +27,16 @@ create_approved_list <- function(new_approved_list){
     }
 
     # Merge existing approved_lists with new approved_lists
-    Brood_approved_list <- dplyr::bind_rows(approved_list$Brood_approved_list, new_approved_list$Brood_approved_list)
-    Capture_approved_list <- dplyr::bind_rows(approved_list$Capture_approved_list, new_approved_list$Capture_approved_list)
-    Individual_approved_list <- dplyr::bind_rows(approved_list$Individual_approved_list, new_approved_list$Individual_approved_list)
-    Location_approved_list <- dplyr::bind_rows(approved_list$Location_approved_list, new_approved_list$Location_approved_list)
+    Brood_approved_list <- dplyr::bind_rows(approved_list$Brood_approved_list,
+                                            new_approved_list$Brood_approved_list)
+    Capture_approved_list <- dplyr::bind_rows(approved_list$Capture_approved_list,
+                                              new_approved_list$Capture_approved_list)
+    Individual_approved_list <- dplyr::bind_rows(approved_list$Individual_approved_list,
+                                                 new_approved_list$Individual_approved_list)
+    Location_approved_list <- dplyr::bind_rows(approved_list$Location_approved_list,
+                                               new_approved_list$Location_approved_list)
+
+    overwrite <- TRUE
 
   } else {
 
@@ -54,13 +60,14 @@ create_approved_list <- function(new_approved_list){
                                              LocationID = NA_character_,
                                              CheckID = NA_character_)
 
+    overwrite <- FALSE
   }
 
   # Combine data-specific approved_lists into one list object
-  approved_list <- list(Brood_wapproved_list = Brood_approved_list,
+  approved_list <- list(Brood_approved_list = Brood_approved_list,
                         Capture_approved_list = Capture_approved_list,
                         Individual_approved_list = Individual_approved_list,
                         Location_approved_list = Location_approved_list)
 
-  usethis::use_data(approved_list)
+  usethis::use_data(approved_list, overwrite=overwrite)
 }
