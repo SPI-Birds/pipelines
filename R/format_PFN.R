0a59fdf872545b8ce30a983d1c5c06af50dd8a97
ChloeRNater <38600576+ChloeRN@users.noreply.github.com>
2020-09-18 12:45:13 +0200

    Update R/format_PFN.R
    Co-authored-by: LiamDBailey <liam.bailey@liamdbailey.com>

--- R/format_PFN.R
+++ R/format_PFN.R
@@ -415,7 +415,7 @@ create_capture_EDM <- function(CMR_data, Primary_data, ReRingTable){
                                           TRUE ~ NA_real_),
                   Tarsus = dplyr::case_when(!is.na(.data$Tarsus_CMR) ~ .data$Tarsus_CMR,
                                             is.na(.data$Tarsus_CMR) & !is.na(.data$Tarsus_B) & .data$Age_observed == 1 ~ .data$Tarsus_B,
-                                            is.na(.data$Tarsus_CMR) & !is.na(.data$Tarsus_B) & .data$Age_observed != 1 ~ NA_real_)) %>%
+                                            TRUE ~ NA_real_)) %>%
 
     ## 7) Exclude entries not included in the standard format
 
