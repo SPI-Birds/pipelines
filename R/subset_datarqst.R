a9e895f21ea9b5872e691101249bf66dc0968e61
ChloeRNater <chloe.r.nater@ntnu.no>
2020-10-25 16:54:02 +0100

    Updates to basic subsetting function
    Tested the subsetting function and made several corrections so that it runs without issues. Included functionality to output either .csv or .RDS (equivalent to run_pipelines()). Added functionality to save a list of data into current R environment (needed for testing). Changed file paths to be compatible with Mac. Changed save path so that all output files are saved into a folder (marked by date) within the directory of the standard data.

--- R/subset_datarqst.R
+++ R/subset_datarqst.R
@@ -15,9 +15,14 @@
 #' @param Species Character vector of six letter species codes. Include all species that are requested.
 #' @param filter Character vector of unique population species combinations (in the format PopID_Species).
 #' Include all unique population species combinations requested.
-#'
-#' @return Create 4 .csv files at the location of the standard data. Files will be given a unique number
-#' based on the date when the subset was performed.
+#' @param output_type is 'R' and can be set to 'csv'. If output_type is 'csv' 4 .csv files will be created in the save path.
+#' If output_type is 'R' an .RDS file will be created in the save path, and an R object in the
+#' running R session if return_R = TRUE.
+#' @param return_R is FALSE. If return R = TRUE, the subset of data is also saved to the current
+#' R Session as an R object (list).
+#' @param save is TRUE. If save = FALSE, the subset of data is not saved into the save path.
+#' @return Creates 1 RDS or 4 .csv files in a folder at the location of the standard data. Folder and files
+#' will be given a unique identifier in the form of the date when the subset was performed.
 #' @export
 #'
 #' @examples
@@ -34,69 +39,115 @@
 #'
 #' }
 
-subset_datarqst <- function(file = file.choose(),
-                            PopID = unique(pop_names$code),
+subset_datarqst <- function(PopID = unique(pop_names$code),
                             Species = unique(Species_codes$Code),
-                            filter = NULL){
+                            filter = NULL,
+                            output_type = "R",
+                            return_R = FALSE,
+                            save = TRUE,
+                            test = FALSE){
 
-  standard_data <- readRDS(file = file)
+  if(!test){
+    standard_data <- readRDS(file = file.choose())
+  }else{
+    standard_data <- test_data
+  }
+
 
   if(!is.null(filter)){
 
-    unique_pops <- unique(stringr::str_split(string = filter, pattern = "_")[[1]])
+    unique_pops <- unique(matrix(unlist(stringr::str_split(string = filter, pattern = "_")), ncol = 2, byrow = T)[,1])
 
     output_brood <- standard_data$Brood_data %>%
-      dplyr::mutate(Pop_sp = paste(PopID, Species, sep = "_")) %>%
-      dplyr::filter(Pop_sp %in% filter)
+      dplyr::mutate(Pop_sp = paste(.data$PopID, .data$Species, sep = "_")) %>%
+      dplyr::filter(.data$Pop_sp %in% filter) %>%
+      dplyr::select(-.data$Pop_sp)
 
     output_capture <- standard_data$Capture_data %>%
-      dplyr::mutate(Pop_sp = paste(CapturePopID, Species, sep = "_")) %>%
-      dplyr::filter(Pop_sp %in% filter)
+      dplyr::mutate(Pop_sp = paste(.data$CapturePopID, .data$Species, sep = "_")) %>%
+      dplyr::filter(.data$Pop_sp %in% filter) %>%
+      dplyr::select(-.data$Pop_sp)
 
     output_individual <- standard_data$Individual_data %>%
-      dplyr::mutate(Pop_sp = paste(PopID, Species, sep = "_")) %>%
-      dplyr::filter(Pop_sp %in% filter)
+      dplyr::mutate(Pop_sp = paste(.data$PopID, .data$Species, sep = "_")) %>%
+      dplyr::filter(.data$Pop_sp %in% filter) %>%
+      dplyr::select(-.data$Pop_sp)
 
     output_location <- standard_data$Location_data %>%
-      dplyr::filter(PopID %in% unique_pops)
+      dplyr::filter(.data$PopID %in% unique_pops)
 
   } else {
 
     output_brood <- standard_data$Brood_data %>%
-      dplyr::filter(PopID %in% {{PopID}} & Species %in% {{Species}})
+      dplyr::filter(.data$PopID %in% {{PopID}} & .data$Species %in% {{Species}})
 
     output_capture <- standard_data$Capture_data %>%
-      dplyr::filter(CapturePopID %in% {{PopID}} & Species %in% {{Species}})
+      dplyr::filter(.data$CapturePopID %in% {{PopID}} & .data$Species %in% {{Species}})
 
     output_individual <- standard_data$Individual_data %>%
-      dplyr::filter(PopID %in% {{PopID}} & Species %in% {{Species}})
+      dplyr::filter(.data$PopID %in% {{PopID}} & .data$Species %in% {{Species}})
 
     if(!all(unique(Species_codes$Code) %in% Species) & all(unique(pop_names$code) %in% PopID)){
 
       PopID <- pop_species_combos %>%
-        dplyr::filter(species %in% Species) %>%
-        dplyr::pull(pop) %>%
+        dplyr::filter(.data$species %in% Species) %>%
+        dplyr::pull(.data$pop) %>%
         unique()
 
     }
 
     output_location <- standard_data$Location_data %>%
-      dplyr::filter(PopID %in% {{PopID}})
+      dplyr::filter(.data$PopID %in% {{PopID}})
 
   }
 
-  message("Output subset as .csv files...")
+  #Combine output into one R object (and return if requested)
+  output_data <- list(Brood_data = output_brood,
+                      Capture_data = output_capture,
+                      Individual_data = output_individual,
+                      Location_data = output_location)
 
-  save_path <- dirname(file)
+  if(return.R){
+    return(output_data)
+  }
 
-  utils::write.csv(x = output_brood, file = paste0(save_path, "\\Brood_data", as.numeric(Sys.Date()), ".csv"), row.names = F)
 
-  utils::write.csv(x = output_capture, file = paste0(save_path, "\\Capture_data", as.numeric(Sys.Date()), ".csv"), row.names = F)
+  if(save){
 
-  utils::write.csv(x = output_individual, file = paste0(save_path, "\\Individual_data", as.numeric(Sys.Date()), ".csv"), row.names = F)
+    #Set save path
 
-  utils::write.csv(x = output_location, file = paste0(save_path, "\\Location_data", as.numeric(Sys.Date()), ".csv"), row.names = F)
+    save_path <- paste0(dirname(file), "/", as.character(Sys.Date()), "_DataRequest")
 
-  invisible(NULL)
+    if(!any(grepl(pattern = paste0(as.character(Sys.Date()), "_DataRequest"), x = list.dirs(dirname(file))))){
+
+      dir.create(save_path)
+
+    }
+
+
+    #Save output as RDS or .csv files
+    if(output_type == "R"){
+
+      message("Output subset as R Data file...")
+
+      saveRDS(output_object, file = paste0(save_path, "/Output_data_", as.character(Sys.Date()), ".RDS"))
+
+    }else{
+
+      message("Output subset as .csv files...")
+
+      utils::write.csv(x = output_brood, file = paste0(save_path, "/Brood_data_", as.character(Sys.Date()), ".csv"), row.names = F)
+
+      utils::write.csv(x = output_capture, file = paste0(save_path, "/Capture_data_", as.character(Sys.Date()), ".csv"), row.names = F)
+
+      utils::write.csv(x = output_individual, file = paste0(save_path, "/Individual_data_", as.character(Sys.Date()), ".csv"), row.names = F)
+
+      utils::write.csv(x = output_location, file = paste0(save_path, "/Location_data_", as.character(Sys.Date()), ".csv"), row.names = F)
+
+    }
+
+  }
+
+invisible(NULL)
 
 }
