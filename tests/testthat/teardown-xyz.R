04d71b979029025b610d52e18729469f63557fdf
Liam Bailey <liam.bailey@liamdbailey.com>
2020-12-11 15:46:08 +0100

    Update teardown process for new tests

--- tests/testthat/teardown-xyz.R
+++ tests/testthat/teardown-xyz.R
@@ -1,9 +1,15 @@
 # Restore approved list
 create_approved_list()
 
-#Move archiving test data back to its original location for future tests
+browser()
+
+#Delete output of archiving tests
 oldwd <- getwd()
 setwd(here::here("./inst/extdata"))
+
+#Delete new archiving created
+system("rm -r ./test_archiving/NEW_testpop/archive")
+system("rm ./test_archiving/NEW_testpop/NEW_ArchiveMetaData.txt")
 
 #Move new data back outside of the folder
 system("mv ./test_archiving/POP_testpop/POP_PrimaryData_XXX.csv ./")
@@ -12,8 +18,8 @@ system("mv ./test_archiving/POP_testpop/POP_PrimaryData_XXX.csv ./")
 system("rm ./test_archiving/POP_testpop/POP_ArchiveMetaData.txt")
 
 #Move current primary and meta data back to original location
-system(paste0("mv ./test_archiving/POP_testpop/archive/", format(Sys.Date(), "%Y_%m_%d"), "/POP_PrimaryData_XXX.csv ./test_archiving/POP_testpop/"))
-system(paste0("mv ./test_archiving/POP_testpop/archive/", format(Sys.Date(), "%Y_%m_%d"), "/POP_ArchiveMetaData.txt ./test_archiving/POP_testpop/"))
+system(paste0("cp ./test_archiving/POP_testpop/archive/", "2019_01_01", "/POP_PrimaryData_XXX.csv ./test_archiving/POP_testpop/"))
+system(paste0("cp ./test_archiving/POP_testpop/archive/", "2019_01_01", "/POP_ArchiveMetaData.txt ./test_archiving/POP_testpop/"))
 
-#Delete archiving folder
-system("rm -r ./test_archiving/POP_testpop/archive")
+#Delete new archiving folder
+system(paste0("rm -r ./test_archiving/POP_testpop/archive/", format(Sys.Date(), format = "%Y_%m_%d")))
