da6b926abfbcf6176bd9802d99029e5c0a4cca47
Liam Bailey <liam.bailey@liamdbailey.com>
2020-12-21 15:54:15 +0100

    Update choose.dir process in test setup

--- tests/testthat/setup-xyz.R
+++ tests/testthat/setup-xyz.R
@@ -1,29 +1,44 @@
 # #Check for existing data file in extdata
 # #Only save this here for efficiency when testing quality check
 # #Do not save to github repo as it contains primary data
-if (file.exists(here::here("inst/extdata/test_data.RDS"))) {
+OS <- tolower(sessionInfo()$running)
 
-  pipeline_output <- readRDS(here::here("inst/extdata/test_data.RDS"))
+#Run pipelines for all populations
+message("Choose the location of the data to run tests...")
 
+if (grepl(pattern = 'mac', x = OS)) {
+
+  path <- tcltk::tk_choose.dir()
+
+} else if (grepl(pattern = 'windows', x = OS)) {
+
+  path <-  utils::choose.dir()
+
+}
+
+#Link to folder with completed pipeline data...
+if ("test_data.RDS" %in% list.files(path)) {
+
+  message("Loading completed pipeline data...")
+
+  pipeline_output <- readRDS(paste0(path, "/test_data.RDS"))
+
+#Or folder with raw data...
 } else {
 
-  #Run pipelines for all populations
-  message("Choose the location of the raw data to run tests...")
-
-  #Determine operating system
-  OS <- tolower(sessionInfo()$running)
+  message("Running all pipelines...")
 
   # Run pipelines depending on operating system
   # (If running on a Mac, pipelines that use Access databases are not run)
   if (grepl(pattern = 'mac', x = OS)) {
-    pipeline_output <- run_pipelines(path = tcltk::tk_choose.dir(),
+    pipeline_output <- run_pipelines(path = path,
                                      PopID = c("SSQ", "BAN", "VEL", "CHO", "MUR", "PIR", "ROU", "MON", "MTV", "MIS", "HOC",
                                                "KEV", "HAR", "PEE", "BOS",
                                                "WYT", "PIL", "EDM"),
                                      output_type = "R")
   } else if (grepl(pattern = 'windows', x = OS)) {
 
-    pipeline_output <- run_pipelines(path = utils::choose.dir(),
+    pipeline_output <- run_pipelines(path = path,
                                      PopID = c("SSQ", "BAN", "VEL", "CHO", "MUR", "PIR", "ROU", "MON", "MTV", "MIS", "HOC",
                                                "HOG", "OOS", "VLI", "BUU", "LIE", "WAR", "WES", "KEV", "HAR", "PEE", "BOS",
                                                "WYT", "PIL", "EDM", "AMM"),
