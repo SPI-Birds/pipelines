3790c2a33f9815a80a7d1c6497836b6c34e1e942
Liam Bailey <liam.bailey@liamdbailey.com>
2020-12-22 10:28:28 +0100

    Remove archiving teardown
    This is now done inside the test function when (if) it is run by the dev

--- tests/testthat/teardown-xyz.R
+++ tests/testthat/teardown-xyz.R
@@ -1,19 +1,2 @@
 # Restore approved list
 create_approved_list()
-
-#Move archiving test data back to its original location for future tests
-oldwd <- getwd()
-setwd(here::here("./inst/extdata"))
-
-#Move new data back outside of the folder
-system("mv ./test_archiving/POP_testpop/POP_PrimaryData_XXX.csv ./")
-
-#Delete new meta-data
-system("rm ./test_archiving/POP_testpop/POP_ArchiveMetaData.txt")
-
-#Move current primary and meta data back to original location
-system(paste0("mv ./test_archiving/POP_testpop/archive/", format(Sys.Date(), "%Y_%m_%d"), "/POP_PrimaryData_XXX.csv ./test_archiving/POP_testpop/"))
-system(paste0("mv ./test_archiving/POP_testpop/archive/", format(Sys.Date(), "%Y_%m_%d"), "/POP_ArchiveMetaData.txt ./test_archiving/POP_testpop/"))
-
-#Delete archiving folder
-system("rm -r ./test_archiving/POP_testpop/archive")
