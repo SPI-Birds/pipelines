c5e92ac05d07e9fbfb8cc9c3e81e36bc19a8e749
Zuzana <zuzulaz@gmail.com>
2021-02-15 14:14:55 +0100

    Add populations
    Add populations ID to pop_codes and to setup-xyz.R for testing.

--- tests/testthat/setup-xyz.R
+++ tests/testthat/setup-xyz.R
@@ -34,14 +34,16 @@ if ("test_data.RDS" %in% list.files(path)) {
     pipeline_output <- run_pipelines(path = path,
                                      PopID = c("SSQ", "BAN", "VEL", "CHO", "MUR", "PIR", "ROU", "MON", "MTV", "MIS", "HOC",
                                                "KEV", "HAR", "PEE", "BOS",
-                                               "WYT", "PIL", "EDM"),
+                                               "WYT", "PIL", "EDM",
+                                               "BAL", "SZE", "VES", "VIL", "GUL"),
                                      output_type = "R")
   } else if (grepl(pattern = 'windows', x = OS)) {
 
     pipeline_output <- run_pipelines(path = path,
                                      PopID = c("SSQ", "BAN", "VEL", "CHO", "MUR", "PIR", "ROU", "MON", "MTV", "MIS", "HOC",
                                                "HOG", "OOS", "VLI", "BUU", "LIE", "WAR", "WES", "KEV", "HAR", "PEE", "BOS",
-                                               "WYT", "PIL", "EDM", "AMM"),
+                                               "WYT", "PIL", "EDM", "AMM",
+                                               "BAL", "SZE", "VES", "VIL", "GUL"),
                                      output_type = "R")
   } else {
     stop(paste0('Operating system ', OS, ' not supported'))
