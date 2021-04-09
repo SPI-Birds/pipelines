c6b2f15954d76307140ecfc8d6b4ecb27586610d
Liam D. Bailey <liam.bailey@liamdbailey.com>
2021-04-09 09:33:44 +0200

    New text

--- R/format_HAR.R
+++ R/format_HAR.R
@@ -1,3 +1,4 @@
+#' SOME NEW TEXT!
 #'This section provides details on data management choices that are unique to
 #'this data. For a general description of the standard protocl please see
 #'\href{https://github.com/SPI-Birds/documentation/blob/master/standard_protocol/SPI_Birds_Protocol_v1.0.0.pdf}{here}.
