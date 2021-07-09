77ea448b6d91112359d2f52721701883dd4db7b0
Liam Bailey <liam.bailey@liamdbailey.com>
2021-07-09 12:15:16 +0200

    ✅ Add test skip in WRS test

--- tests/testthat/test-WRS.R
+++ tests/testthat/test-WRS.R
@@ -1,6 +1,6 @@
-pipeline_output <- format_WRS(db = paste0(data_path, "/WRS_Warsaw_Poland"))
+testthat::skip_if(!exists("data_path"))
 
-context("Run data quality check on Warswaw, Poland, pipeline output")
+pipeline_output <- format_WRS(db = paste0(data_path, "/WRS_Warsaw_Poland"))
 
 test_that("WRS outputs all files...", {
 
