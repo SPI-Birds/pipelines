b5bfdd0598a6c187b36605b0a299ac6ae754895c
Chris Tyson <cwtyson@ucdavis.edu>
2021-07-30 15:10:04 +0200

    :rocket: Update data templates

--- R/create_data_table_templates.R
+++ R/create_data_table_templates.R
@@ -3,7 +3,7 @@
 ## Dummy values are entered for columns that cannot contain NAs
 
 ## Brood data template
-brood_data_template <- tibble::tibble(BroodID = as.character("POP-1"), ## Missing data not allowed
+brood_data_template <- tibble::tibble(BroodID = as.character(paste0("POP-", 1:7)), ## Missing data not allowed
                                       PopID = as.character("POP"), ## Missing data not allowed
                                       BreedingSeason = as.integer(2021), ## Missing data not allowed
                                       Species = as.character("SPECIES"), ## Missing data not allowed
@@ -11,40 +11,40 @@ brood_data_template <- tibble::tibble(BroodID = as.character("POP-1"), ## Missin
                                       LocationID = NA_character_,
                                       FemaleID = NA_character_,
                                       MaleID = NA_character_,
-                                      ClutchType_observed = c("first","second","replacement",NA,NA,NA),
-                                      ClutchType_calculated = c("first","second","replacement",NA,NA,NA),
+                                      ClutchType_observed = c("first","second","replacement",NA,NA,NA,NA),
+                                      ClutchType_calculated = c("first","second","replacement",NA,NA,NA,NA),
                                       LayDate_observed = as.Date(NA),
                                       LayDate_min = as.Date(NA),
                                       LayDate_max = as.Date(NA),
                                       ClutchSize_observed = NA_integer_,
                                       ClutchSize_min = NA_integer_,
-                                      ClutchSize_max = NA_real_,
+                                      ClutchSize_max = NA_integer_,
                                       HatchDate_observed = as.Date(NA),
                                       HatchDate_min = as.Date(NA),
                                       HatchDate_max = as.Date(NA),
                                       BroodSize_observed = NA_integer_,
                                       BroodSize_min = NA_integer_,
-                                      BroodSize_max = NA_real_,
+                                      BroodSize_max = NA_integer_,
                                       FledgeDate_observed = as.Date(NA),
                                       FledgeDate_min = as.Date(NA),
                                       FledgeDate_max = as.Date(NA),
                                       NumberFledged_observed= NA_integer_,
                                       NumberFledged_min = NA_integer_,
-                                      NumberFledged_max = NA_real_,
+                                      NumberFledged_max = NA_integer_,
                                       AvgEggMass = NA_real_,
                                       NumberEggs = NA_integer_,
                                       AvgChickMass = NA_real_,
                                       NumberChicksMass = NA_integer_,
                                       AvgTarsus = NA_real_,
                                       NumberChicksTarsus = NA_integer_,
-                                      OriginalTarsusMethod = c("Alternative", "Standard", "Oxford", NA, NA, NA),
-                                      ExperimentID = c("PHENOLOGY","COHORT", "PARENTAGE", "SURVIVAL", "OTHER", NA))
+                                      OriginalTarsusMethod = c("Alternative", "Standard", "Oxford", NA, NA, NA,NA),
+                                      ExperimentID = c("PHENOLOGY","COHORT", "PARENTAGE", "SURVIVAL", "OTHER", "SURVIVAL; OTHER", NA))
 
 ## Save
 save(brood_data_template, file = "./data/Brood_data_template.RData")
 
 ## Capture data template
-capture_data_template <- tibble::tibble(CaptureID = as.character("POP"), ## Missing data not allowed
+capture_data_template <- tibble::tibble(CaptureID = as.character(paste0("POP-", 1:6)), ## Missing data not allowed
                                         IndvID = as.character("INDV00"), ## Missing data not allowed
                                         Species = as.character("SPECIES"), ## Missing data not allowed
                                         Sex_observed = NA_character_,
@@ -73,7 +73,7 @@ save(capture_data_template, file = "./data/Capture_data_template.RData")
 
 
 ## Individual data template
-individual_data_template <- tibble::tibble(IndvID = as.character("INDV00"), ## Missing data not allowed
+individual_data_template <- tibble::tibble(IndvID = as.character(paste0("INDV0", 1:4)), ## Missing data not allowed
                                            Species = as.character("SPECIES"), ## Missing data not allowed
                                            PopID = as.character("POP"), ## Missing data not allowed
                                            BroodIDLaid = NA_character_,
