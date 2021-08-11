5aaa3fb4959f07b307e8444b9ba7c7a63a290f06
Chris Tyson <cwtyson@ucdavis.edu>
2021-08-11 10:42:27 +0200

    :truck: Move VOM test to correct folder

--- tests/testthat/test-VOM.R
+++ tests/testthat/test-VOM.R
@@ -245,16 +245,16 @@ test_that("Column classes are as expected", {
 test_that("ID columns match the expected format for the pipeline", {
 
   ## FemaleID format is as expected
-  test_ID_format(pipeline_output, ID_col = "FemaleID", ID_format = "^2{1}[:alpha:]{2}[:digit:]{5}")
+  test_ID_format(pipeline_output, ID_col = "FemaleID", ID_format = "^2{1}[:alpha:]{2}[:digit:]{5}$")
 
   ## MaleID format is as expected
-  test_ID_format(pipeline_output, ID_col = "MaleID", ID_format = "^2{1}[:alpha:]{2}[:digit:]{5}")
+  test_ID_format(pipeline_output, ID_col = "MaleID", ID_format = "^2{1}[:alpha:]{2}[:digit:]{5}$")
 
   ## IndvID format in Capture data  is as expected
-  test_ID_format(pipeline_output, ID_col = "C-IndvID", ID_format = "^2{1}[:alpha:]{2}[:digit:]{5}")
+  test_ID_format(pipeline_output, ID_col = "C-IndvID", ID_format = "^2{1}[:alpha:]{2}[:digit:]{5}$")
 
   ## IndvID format in Individual data is as expected
-  test_ID_format(pipeline_output, ID_col = "I-IndvID", ID_format = "^2{1}[:alpha:]{2}[:digit:]{5}")
+  test_ID_format(pipeline_output, ID_col = "I-IndvID", ID_format = "^2{1}[:alpha:]{2}[:digit:]{5}$")
 
 })
 
