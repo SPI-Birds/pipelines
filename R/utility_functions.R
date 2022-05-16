618cc8a100ca8332235af49d233c342552196cd2
Stefan Vriend <svriend@gmail.com>
2022-05-16 16:32:17 +0200

    :construction: Create standard utility functions
    Store all standard utility functions in one file (utility_functions.R).

---
+++ R/utility_functions.R
@@ -0,0 +1,14 @@
+#--------------------------------------------------------------#
+# Standard utility functions
+#
+# Standard utility functions are functions that generate additional variables that are not part of standard format but can be added to pipeline outputs when requested by the user. Often these variables are not obtained from direct observations in the field but derived from other variables (e.g., an individual's age is derived from its year of ringing/birth and the current year).
+#
+# Optional variables:
+# - breedingSeason, calc_season()
+# - calculatedClutchType, calc_clutchtype()
+# - nestAttemptNumber, calc_nestattempt()
+# - calculatedSex, calc_sex()
+# - exactAge, calc_age()
+# - minimumAge, calc_age()
+#--------------------------------------------------------------#
+
