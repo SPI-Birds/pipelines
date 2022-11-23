3bba9e11e5444bb66e152583c491b0d0323d5a35
Stefan Vriend <svriend@gmail.com>
2022-11-23 10:56:16 +0100

    :memo: Update utility function documentation

--- R/utility_functions.R
+++ R/utility_functions.R
@@ -1,7 +1,7 @@
 #--------------------------------------------------------------#
 # Standard utility functions
 #
-# Standard utility functions are functions that generate additional variables that are not part of standard format but can be added to pipeline outputs when requested by the user. Often these variables are not obtained from direct observations in the field but derived from other variables (e.g., an individual's age is derived from its year of ringing/birth and the current year).
+# Standard utility functions are functions that generate additional variables that are not part of standard format but can be added to pipeline outputs when requested by the user. Often these variables are not obtained from direct observations in the field but derived from other variables (e.g., an individual's age is derived from its date of ringing and the current date).
 #
 # Optional variables:
 # - breedingSeason, calc_season()
