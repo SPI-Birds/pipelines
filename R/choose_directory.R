7563975005e9ba83aaf8e6a2861b3c43c2b58091
Stefan Vriend <svriend@gmail.com>
2024-07-31 09:28:01 +0200

    :bug:Use tcltk::tk_choose.dir() instead of choose.dir() on Windows
    choose.dir() is returning NA, this is a known bug and not solved yet

--- R/choose_directory.R
+++ R/choose_directory.R
@@ -6,9 +6,20 @@
 #' @export
 
 choose_directory <- function() {
-  if (exists('choose.dir')) {
-    utils::choose.dir()
-  } else {
-    tcltk::tk_choose.dir()
-  }
+
+  # choose.dir() results in NA in Rstudio, since R 4.3.0
+  # use tcltk::tk_choose.dir() for all OS for the time being
+
+  tcltk::tk_choose.dir()
+
+  # if (exists('choose.dir')) {
+  #
+  #   utils::choose.dir()
+  #
+  # } else {
+  #
+  #   tcltk::tk_choose.dir()
+  #
+  # }
+
 }
