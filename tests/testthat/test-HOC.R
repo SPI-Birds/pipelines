fd05c9705a8ee3de802a925bb6a314c9d61818e1
Ronald van der Woude <ronnie.vd.woude@gmail.com>
2025-11-18 16:12:15 +0100

    Updated the test files for HOC

--- tests/testthat/test-HOC.R
+++ tests/testthat/test-HOC.R
@@ -3,23 +3,20 @@ testthat::skip_if(!exists("data_path"))
 pipeline_output <- format_HOC(db = paste0(data_path, "/HOC_Hochstadt_Germany"))
 
 test_that("HOC outputs all files...", {
-
   expect_true(all("HOC" %in% pipeline_output$Brood_data$PopID))
   expect_true(all("HOC" %in% pipeline_output$Capture_data$CapturePopID))
   expect_true(all("HOC" %in% pipeline_output$Individual_data$PopID))
   expect_true(all("HOC" %in% pipeline_output$Location_data$PopID))
   expect_true(pipeline_output$protocol_version == "1.0.0")
-
 })
 
 test_that("Brood_data returns an expected outcome...", {
+  # We want to run tests for all possible outcomes of ClutchType_calculated
 
-  #We want to run tests for all possible outcomes of ClutchType_calculated
-
-  #Take a subset of only HOC data
+  # Take a subset of only HOC data
   HOC_data <- dplyr::filter(pipeline_output$Brood_data, PopID %in% "HOC")
 
-  #Test 1: Brood where clutch type = first
+  # Test 1: Brood where clutch type = first
   expect_equal(subset(HOC_data, BroodID == "2016-H101-1")$Species, "PARMAJ")
   expect_equal(subset(HOC_data, BroodID == "2016-H101-1")$ClutchType_calculated, "first")
   expect_equal(subset(HOC_data, BroodID == "2016-H101-1")$LayDate, as.Date("2016-04-22"))
@@ -29,7 +26,7 @@ test_that("Brood_data returns an expected outcome...", {
   expect_equal(subset(HOC_data, BroodID == "2016-H101-1")$AvgChickMass, NA_real_)
   expect_equal(subset(HOC_data, BroodID == "2016-H101-1")$AvgTarsus, NA_real_)
 
-  #Test 2: Brood where clutch type = replacement (because first is known to have failed)
+  # Test 2: Brood where clutch type = replacement (because first is known to have failed)
   expect_equal(subset(HOC_data, BroodID == "2016-H111-2")$Species, "PARMAJ")
   expect_equal(subset(HOC_data, BroodID == "2016-H111-2")$ClutchType_calculated, "replacement")
   expect_equal(subset(HOC_data, BroodID == "2016-H111-2")$LayDate, as.Date("2016-05-30"))
@@ -39,7 +36,7 @@ test_that("Brood_data returns an expected outcome...", {
   expect_equal(round(subset(HOC_data, BroodID == "2016-H111-2")$AvgChickMass, 2), 13.65)
   expect_equal(round(subset(HOC_data, BroodID == "2016-H111-2")$AvgTarsus, 2), 18.20)
 
-  #Test 3: Brood where clutch type = replacement (past the cutoff)
+  # Test 3: Brood where clutch type = replacement (past the cutoff)
   expect_equal(subset(HOC_data, BroodID == "2017-H101-2")$Species, "PARMAJ")
   expect_equal(subset(HOC_data, BroodID == "2017-H101-2")$ClutchType_calculated, "replacement")
   expect_equal(subset(HOC_data, BroodID == "2017-H101-2")$LayDate, as.Date("2017-06-03"))
@@ -49,7 +46,7 @@ test_that("Brood_data returns an expected outcome...", {
   expect_equal(round(subset(HOC_data, BroodID == "2017-H101-2")$AvgChickMass, 2), 12.00)
   expect_equal(round(subset(HOC_data, BroodID == "2017-H101-2")$AvgTarsus, 2), 15.92)
 
-  #Test 4: Brood where clutch type = second
+  # Test 4: Brood where clutch type = second
   expect_equal(subset(HOC_data, BroodID == "2017-H71-2")$Species, "PARMAJ")
   expect_equal(subset(HOC_data, BroodID == "2017-H71-2")$ClutchType_calculated, "second")
   expect_equal(subset(HOC_data, BroodID == "2017-H71-2")$LayDate, as.Date("2017-05-31"))
@@ -58,135 +55,203 @@ test_that("Brood_data returns an expected outcome...", {
   expect_equal(subset(HOC_data, BroodID == "2017-H71-2")$NumberFledged, 4L)
   expect_equal(round(subset(HOC_data, BroodID == "2017-H71-2")$AvgChickMass, 2), 13.75)
   expect_equal(round(subset(HOC_data, BroodID == "2017-H71-2")$AvgTarsus, 2), 18.55)
-
 })
 
 test_that("Individual data returns an expected outcome...", {
+  # We want to run a test for each sex for individuals caught as adults and chicks
 
-  #We want to run a test for each sex for individuals caught as adults and chicks
-
-  #Take a subset of only HOC data
+  # Take a subset of only HOC data
   HOC_data <- dplyr::filter(pipeline_output$Individual_data, PopID %in% "HOC")
 
-  #Test 1: First as adult
+  # Test 1: First as adult
   expect_equal(subset(HOC_data, IndvID == "C1X0940")$Sex, "F")
   expect_equal(subset(HOC_data, IndvID == "C1X0940")$Species, "PARMAJ")
-  #They should have no BroodIDLaid or Fledged because she was never caught as a chick
+  # They should have no BroodIDLaid or Fledged because she was never caught as a chick
   expect_equal(subset(HOC_data, IndvID == "C1X0940")$BroodIDLaid, NA_character_)
   expect_equal(subset(HOC_data, IndvID == "C1X0940")$BroodIDFledged, NA_character_)
-  #Ring age and season are as expected
+  # Ring age and season are as expected
   expect_equal(subset(HOC_data, IndvID == "C1X0940")$RingSeason, 2014L)
   expect_equal(subset(HOC_data, IndvID == "C1X0940")$RingAge, "adult")
 
-  #Test 2: Caught first as chick
+  # Test 2: Caught first as chick
   expect_equal(subset(HOC_data, IndvID == "C1X0972")$Sex, NA_character_)
   expect_equal(subset(HOC_data, IndvID == "C1X0972")$Species, "PARMAJ")
-  #Check that BroodIDLaid/Fledged are as expected
+  # Check that BroodIDLaid/Fledged are as expected
   expect_equal(subset(HOC_data, IndvID == "C1X0972")$BroodIDLaid, "2014-H61-1")
   expect_equal(subset(HOC_data, IndvID == "C1X0972")$BroodIDFledged, "2014-H61-1")
-  #Ring season is as expected
+  # Ring season is as expected
   expect_equal(subset(HOC_data, IndvID == "C1X0972")$RingSeason, 2014)
   expect_equal(subset(HOC_data, IndvID == "C1X0972")$RingAge, "chick")
-
 })
 
 test_that("Capture data returns an expected outcome...", {
-
-  #Take a subset of only HOC data
+  # Take a subset of only HOC data
   HOC_data <- dplyr::filter(pipeline_output$Capture_data, CapturePopID %in% "HOC")
 
-  #Test 1: Individual ringed as a chick
-  #Test there are the correct number of capture records
+  # Test 1: Individual ringed as a chick
+  # Test there are the correct number of capture records
   expect_equal(nrow(subset(HOC_data, IndvID == "C3F8488")), 5)
-  #Test that the first and 5th capture are as expected
+  # Test that the first and 5th capture are as expected
   expect_equal(subset(HOC_data, IndvID == "C3F8488")$CaptureDate[1], as.Date("2014-05-18"))
   expect_equal(subset(HOC_data, IndvID == "C3F8488")$CaptureDate[5], as.Date("2016-06-27"))
-  #Test that age observed is as expected on first capture
-  #Test that age observed is as expected on 5th capture
+  # Test that age observed is as expected on first capture
+  # Test that age observed is as expected on 5th capture
   expect_equal(subset(HOC_data, IndvID == "C3F8488")$Age_observed[1], 1L)
   expect_equal(subset(HOC_data, IndvID == "C3F8488")$Age_observed[5], 4L)
-  #Test that age calculated is correct on first capture and last capture
+  # Test that age calculated is correct on first capture and last capture
   expect_equal(subset(HOC_data, IndvID == "C3F8488")$Age_calculated[1], 1L)
   expect_equal(subset(HOC_data, IndvID == "C3F8488")$Age_calculated[5], 7L)
 
-  #Test 2: Individual caught only as adult
-  #Test it has the correct number of capture records
+  # Test 2: Individual caught only as adult
+  # Test it has the correct number of capture records
   expect_equal(nrow(subset(HOC_data, IndvID == "C3K7291")), 7)
-  #Test that the first capture is as expected
+  # Test that the first capture is as expected
   expect_equal(subset(HOC_data, IndvID == "C3K7291")$CaptureDate[1], as.Date("2016-05-09"))
-  #Test that the 7th capture is as expected
+  # Test that the 7th capture is as expected
   expect_equal(subset(HOC_data, IndvID == "C3K7291")$CaptureDate[7], as.Date("2019-05-08"))
-  #Test that first and last age observed is as expected
+  # Test that first and last age observed is as expected
   expect_equal(subset(HOC_data, IndvID == "C3K7291")$Age_observed[1], 4L)
   expect_equal(subset(HOC_data, IndvID == "C3K7291")$Age_observed[7], 4L)
-  #Test that first and last age calculated is as expected
+  # Test that first and last age calculated is as expected
   expect_equal(subset(HOC_data, IndvID == "C3K7291")$Age_calculated[1], 4L)
   expect_equal(subset(HOC_data, IndvID == "C3K7291")$Age_calculated[7], 10L)
 
-  #Test 3: Individual caught as 1st year adult pre 2019
-  #This individual should be treated as EURING 4
-  #Test it has the correct number of capture records
+  # Test 3: Individual caught as 1st year adult pre 2019
+  # This individual should be treated as EURING 4
+  # Test it has the correct number of capture records
   expect_equal(nrow(subset(HOC_data, IndvID == "C3K7272")), 5)
-  #Test that the first capture is as expected
+  # Test that the first capture is as expected
   expect_equal(subset(HOC_data, IndvID == "C3K7272")$CaptureDate[1], as.Date("2016-03-08"))
-  #Test that the 7th capture is as expected
+  # Test that the 7th capture is as expected
   expect_equal(subset(HOC_data, IndvID == "C3K7272")$CaptureDate[5], as.Date("2019-05-09"))
-  #Test that first and last age observed is as expected
+  # Test that first and last age observed is as expected
   expect_equal(subset(HOC_data, IndvID == "C3K7272")$Age_observed[1], 4L)
   expect_equal(subset(HOC_data, IndvID == "C3K7272")$Age_observed[5], 4L)
-  #Test that first and last age calculated is as expected
+  # Test that first and last age calculated is as expected
   expect_equal(subset(HOC_data, IndvID == "C3K7272")$Age_calculated[1], 4L)
   expect_equal(subset(HOC_data, IndvID == "C3K7272")$Age_calculated[5], 10L)
 
-  #Test 4: Individual caught as 1st year adult post 2019
-  #This individual should be treated as EURING 5
-  #Test it has the correct number of capture records
+  # Test 4: Individual caught as 1st year adult post 2019
+  # This individual should be treated as EURING 5
+  # Test it has the correct number of capture records
   expect_equal(nrow(subset(HOC_data, IndvID == "C5E9102")), 2)
-  #Test that the first capture is as expected
+  # Test that the first capture is as expected
   expect_equal(subset(HOC_data, IndvID == "C5E9102")$CaptureDate[1], as.Date("2019-03-16"))
-  #Test that the 7th capture is as expected
+  # Test that the 7th capture is as expected
   expect_equal(subset(HOC_data, IndvID == "C5E9102")$CaptureDate[2], as.Date("2019-05-08"))
-  #Test that first and last age observed is as expected
+  # Test that first and last age observed is as expected
   expect_equal(subset(HOC_data, IndvID == "C5E9102")$Age_observed[1], 5L)
   expect_equal(subset(HOC_data, IndvID == "C5E9102")$Age_observed[2], 4L)
-  #Test that first and last age calculated is as expected
+  # Test that first and last age calculated is as expected
   expect_equal(subset(HOC_data, IndvID == "C5E9102")$Age_calculated[1], 4L)
   expect_equal(subset(HOC_data, IndvID == "C5E9102")$Age_calculated[2], 4L)
 
-  #Test 5: Individual found dead in DeadCapture ID table
-  #Test it has the correct number of capture records
+  # Test 5: Individual found dead in DeadCapture ID table
+  # Test it has the correct number of capture records
   expect_equal(nrow(subset(HOC_data, IndvID == "C5E8940")), 3)
-  #Test that the first capture is as expected
+  # Test that the first capture is as expected
   expect_equal(subset(HOC_data, IndvID == "C5E8940")$CaptureDate[1], as.Date("2019-05-13"))
-  #Test that the 7th capture is as expected
+  # Test that the 7th capture is as expected
   expect_equal(subset(HOC_data, IndvID == "C5E8940")$CaptureDate[3], as.Date("2019-05-23"))
-  #Test that first and last age observed is as expected
+  # Test that first and last age observed is as expected
   expect_equal(subset(HOC_data, IndvID == "C5E8940")$Age_observed[1], 1L)
   expect_equal(subset(HOC_data, IndvID == "C5E8940")$Age_observed[3], 1L)
-  #Test that first and last age calculated is as expected
+  # Test that first and last age calculated is as expected
   expect_equal(subset(HOC_data, IndvID == "C5E8940")$Age_calculated[1], 1L)
   expect_equal(subset(HOC_data, IndvID == "C5E8940")$Age_calculated[3], 1L)
-
 })
 
 test_that("Location_data returns an expected outcome...", {
+  # We want to run tests for nest boxes (there are no mistnets)
 
-  #We want to run tests for nest boxes (there are no mistnets)
-
-  #Take a subset of only NIOO data
+  # Take a subset of only NIOO data
   HOC_data <- dplyr::filter(pipeline_output$Location_data, PopID %in% "HOC")
 
-  #Test 1: Nestbox check
+  # Test 1: Nestbox check
   expect_true(subset(HOC_data, LocationID == "H1")$LocationType == "NB")
-  #Expect LocationID and NestboxID are the same
+  # Expect LocationID and NestboxID are the same
   expect_true(subset(HOC_data, LocationID == "H1")$NestboxID == "H1")
-  #Expect Start and EndSeason is as expected
+  # Expect Start and EndSeason is as expected
   expect_equal(subset(HOC_data, LocationID == "H1")$StartSeason, 2014L)
   expect_equal(subset(HOC_data, LocationID == "H1")$EndSeason, NA_integer_)
-  #Check that LocationID is in the expected PopID
+  # Check that LocationID is in the expected PopID
   expect_equal(subset(HOC_data, LocationID == "H1")$PopID, "HOC")
-  #Check that latitude and longitude are as expected
+  # Check that latitude and longitude are as expected
   expect_equal(round(subset(HOC_data, LocationID == "H1")$Latitude, 2) %>% setNames(nm = NULL), 11.26)
   expect_equal(round(subset(HOC_data, LocationID == "H1")$Longitude, 2) %>% setNames(nm = NULL), 48.06)
+})
 
+## General tests
+
+test_that("Expected columns are present", {
+  ## Will fail if not all the expected columns are present
+
+  ## Brood data: Test that all columns are present
+  test_col_present(pipeline_output, "Brood", pipeline_output$protocol_version)
+
+  ## Capture data: Test that all columns are present
+  test_col_present(pipeline_output, "Capture", pipeline_output$protocol_version)
+
+  ## Individual data: Test that all columns are present
+  test_col_present(pipeline_output, "Individual", pipeline_output$protocol_version)
+
+  ## Location data: Test that all columns are present
+  test_col_present(pipeline_output, "Location", pipeline_output$protocol_version)
+})
+
+test_that("Column classes are as expected", {
+  ## Will fail if columns that are shared by the output and the templates have different classes.
+
+  ## Brood data: Test that all column classes are expected
+  test_col_classes(pipeline_output, "Brood", pipeline_output$protocol_version)
+
+  ## Capture data: Test that all column classes are expected
+  test_col_classes(pipeline_output, "Capture", pipeline_output$protocol_version)
+
+  ## Individual data: Test that all column classes are expected
+  test_col_classes(pipeline_output, "Individual", pipeline_output$protocol_version)
+
+  ## Location data: Test that all column classes are expected
+  test_col_classes(pipeline_output, "Location", pipeline_output$protocol_version)
+})
+
+test_that("Key columns only contain unique values", {
+  ## BroodID has only unique values
+  test_unique_values(pipeline_output, "BroodID")
+
+  ## CaptureID has only unique values
+  test_unique_values(pipeline_output, "CaptureID")
+
+  ## PopID-IndvID has only unique values
+  test_unique_values(pipeline_output, "PopID-IndvID")
+})
+
+
+test_that("Key columns in each table do not have NAs", {
+  ## Brood
+  test_NA_columns(pipeline_output, "Brood")
+
+  ## Capture
+  test_NA_columns(pipeline_output, "Capture")
+
+  ## Individual
+  test_NA_columns(pipeline_output, "Individual")
+
+  ## Location
+  test_NA_columns(pipeline_output, "Location")
+})
+
+
+test_that("Categorical columns do not have unexpected values", {
+  ## Brood
+  test_category_columns(pipeline_output, "Brood")
+
+  ## Capture
+  test_category_columns(pipeline_output, "Capture")
+
+  ## Individual
+  test_category_columns(pipeline_output, "Individual")
+
+  ## Location
+  test_category_columns(pipeline_output, "Location")
 })
